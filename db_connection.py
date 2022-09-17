ae065251df4fb8fbf2c57e93eb776ff9d16aa1f3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-17 15:34:06 +0100

    Modified DB connection file to be a module that can be exported into other scripts

--- db_connection.py
+++ db_connection.py
@@ -5,40 +5,15 @@ import mysql.connector
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
-# Create Database Connection
-creds = {'usr': 'root',
-        'pwd': 'p',
-        'hst': '127.0.0.1',
-        'prt': '3306',
-        'dbn': 'test'}
-creds2 = {'usr': os.getenv("USR"),
-        'pwd': 'p',
+
+connection_credentials = {'usr': os.getenv("USR"),
+        'pwd': os.getenv("PASSWORD"),
         'hst': os.getenv("HOST"),
         'prt': os.getenv("PORT"),
         'dbn': os.getenv("DATABASE")}
-connstr = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}'
-# # engine = create_engine(connstr.format(**creds))
-# # brand_prefix = pd.read_sql('brand', engine, columns = ["prefix"], index_col="name")
-# # print(brand_prefix)
 
-#  # PYTHON FUNCTION TO CONNECT TO THE MYSQL DATABASE AND
-#  # RETURN THE SQLACHEMY ENGINE OBJECT
-def get_connection():
-# engine = create_engine(connstr.format(**creds))
-    # return create_engine(connstr.format(**creds))
-    return create_engine(connstr.format(**creds2))
-    # return create_engine(connstr = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}')
-    print(creds)
-    print(creds2)
+connection_string = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}'
 
-if __name__ == '__main__':
-    try:
-    # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-        engine = get_connection()
-        brand_prefix = pd.read_sql('brand', engine)
-        print(brand_prefix)
-        # print(f"Connection to the {hst} for user {usr} created successfully.")
-    except Exception as ex:
-        print("Connection could not be made due to the following error: \n", ex)
-
-# Create Database Connection 
+# Create Database Connection
+def create_connection():
+    return create_engine(connection_string.format(**connection_credentials))
