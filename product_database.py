fea64f679c6a16614a768dd035aa760d7e3f35b3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-27 21:21:14 +0100

    Created file to import products into db

---
+++ product_database.py
@@ -0,0 +1,17 @@
+# import pandas as pd
+import pandas as pd
+import sqlalchemy as sqlalchemy
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import mysql.connector
+import db_connection
+import os
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+try:
+# GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
+    engine = db_connection.create_connection()
+    test_table = pd.read_sql(os.getenv('test_table'),engine)
+    print(test_table)
+except Exception as ex:
+    print("Connection could not be made due to the following error: \n", ex)
