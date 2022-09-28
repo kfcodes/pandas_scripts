b0f4f1139e754a1d21066b032a8c226d070c6078
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-09-28 20:45:18 +0100

    Select specific columns from dataframe

--- product_database.py
+++ product_database.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+# Select the desired columns from the input file
 import pandas as pd
 import sqlalchemy as sqlalchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
@@ -12,4 +12,6 @@ database_output_file = os.getenv('DATABASE_FILE')
 
 sage_dataframe = pd.read_excel(database_output_file)
 
-print(database_output_file)
+sage_column_data = sage_dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
+
+print(sage_column_data)
