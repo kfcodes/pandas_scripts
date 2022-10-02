b68f7c01e8ebc29105d97be8a0c7f2a7c92ce2ec
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-10-02 21:11:58 +0100

    prduction schedule file reads all prducts from sql

--- production_schedule.py
+++ production_schedule.py
@@ -1,17 +1,17 @@
-# Created production schedule file that connects to db
+# import products from database
 import pandas as pd
 import sqlalchemy as sqlalchemy
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 import mysql.connector
 import db_connection
 import os
+import numpy as np
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-try:
-# GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-    engine = db_connection.create_connection()
-    test_table = pd.read_sql(os.getenv('test_table'),engine)
-    print(test_table)
-except Exception as ex:
-    print("Connection could not be made due to the following error: \n", ex)
+engine = db_connection.create_connection()
+
+products = pd.read_sql(os.getenv('pdb_table'), engine, columns=[os.getenv('Column_1'), os.getenv('Column_2')])
+print(products)
