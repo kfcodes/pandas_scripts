9d4fef38ffa1c1e305744c1af71153dcf2696fea
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-11-26 18:45:29 +0000

    Retrieve selected data from bom dictionary

--- process_xml.py
+++ process_xml.py
@@ -3,4 +3,6 @@ import xmltodict
 with open('../test.xml') as fd:
     xml_dict = xmltodict.parse(fd.read(), process_namespaces=True)
     for product in xml_dict['BOMRecords']['BOMRecord']:
-        print(product)
+        product_code = product['Reference']
+        product_description = product['Description']
+        print( product_code + ' - ' + product_description )
