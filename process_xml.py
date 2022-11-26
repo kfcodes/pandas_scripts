6bf90f18a0a5cdcd1901aff9c274e578a539faad
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-11-26 18:35:54 +0000

    file loops through dictionary for bom data

--- process_xml.py
+++ process_xml.py
@@ -2,4 +2,5 @@ import xmltodict
 
 with open('../test.xml') as fd:
     xml_dict = xmltodict.parse(fd.read(), process_namespaces=True)
-    print(xml_dict)
+    for product in xml_dict['BOMRecords']['BOMRecord']:
+        print(product)
