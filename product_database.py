e0fb3b694ee29fd616925392a0c0d4f9cfdacfba
kfcodes <87446283+kfcodes@users.noreply.github.com>
2022-12-29 20:08:47 +0000

    Removed unused code and refactored the product database file

--- product_database.py
+++ product_database.py
@@ -1,28 +1,19 @@
-# Put db connection in try catch block
 import pandas as pd
 import sqlalchemy as sqlalchemy
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-import mysql.connector
-import db_connection
+from sql import write_pandas_dataframe_to_sql
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 
 database_output_file = os.getenv('DATABASE_FILE')
 
-sage_dataframe = pd.read_excel(database_output_file)
+product_database_dataframe = pd.read_excel(database_output_file)
 
-sage_column_data = sage_dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
+sage_column_data = product_database_dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
 
 correct_column_names = sage_column_data.rename(columns={os.getenv('Column1'):os.getenv('Column_1'),os.getenv('Column2'):os.getenv('Column_2'),os.getenv('Column3'):os.getenv('Column_3'),os.getenv('Column4'):os.getenv('Column_4'),})
 
 correct_column_names[os.getenv('Column_1')] = correct_column_names[os.getenv('Column_1')].str.lower()
 correct_column_names[os.getenv('Column_2')] = correct_column_names[os.getenv('Column_2')].str.lower()
 
-try:
-# GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-    engine = db_connection.create_connection()
-    correct_column_names.to_sql(os.getenv('pdb_table'), engine, if_exists='append', index=False)
-    print("Data was inserted into the database")
-except Exception as ex:
-    print("Connection could not be made due to the following error: \n", ex)
+write_pandas_dataframe_to_sql.write_to_database(correct_column_names, 'pdb_table')
