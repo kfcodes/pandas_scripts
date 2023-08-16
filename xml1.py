ef7a9394398c31372102f8d3e3bb13f15b0deec8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-16 12:08:38 +0100

    Printing the Product information to the console

--- xml1.py
+++ xml1.py
@@ -6,10 +6,9 @@ load_dotenv(".env")
 bom = os.getenv('XML')
 
 # The loop new extracts the components from the xml file
-with open(product) as fd:
+with open(p) as fd:
     xml_dict = xmltodict.parse(fd.read(), process_namespaces=True)
-    for producti in xml_dict['Record']
+    for product in xml_dict['BOMRecords']['BOMRecord']:
         product_code = product['Reference']
         product_description = product['Description']
-        # print( product_code + ' - ' + product_description )
-            print(product)
+        print( product_code + ' - ' + product_description )
