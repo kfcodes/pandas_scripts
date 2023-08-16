6476df438dd2ca19ece80bdfe1ca4713714378ae
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-16 14:17:44 +0100

    Removed unused code from db connection module

--- modules/sql/db_connection.py
+++ modules/sql/db_connection.py
@@ -2,7 +2,6 @@ import sqlalchemy as sqlalchemy
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-
 load_dotenv(".env")
 
 connection_credentials = {'usr': os.getenv("USR"),
@@ -11,9 +10,7 @@ connection_credentials = {'usr': os.getenv("USR"),
         'prt': os.getenv("PORT"),
         'dbn': os.getenv("DATABASE")}
 
-#connection_string = 'mysql+mysqlconnector://{usr}:{pwd}@{hst}:{prt}/{dbn}'
 connection_string = 'mariadb+pymysql://{usr}:{pwd}@{hst}:{prt}/{dbn}'
 
-# Create Database Connection
 def database_connection():
     return create_engine(connection_string.format(**connection_credentials))
