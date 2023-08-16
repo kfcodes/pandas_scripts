6307f3fe712f6a3b0de01f8b90207ef48867db05
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-16 14:27:10 +0100

    Improved function to write data to the DB

--- modules/sql/write_pandas_dataframe_to_sql.py
+++ modules/sql/write_pandas_dataframe_to_sql.py
@@ -1,14 +1,13 @@
-import sqlalchemy as sqlalchemy
-from sql import db_connection
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
+import pandas as pd
+from db_connection import database_connection
+import sqlalchemy as sqlalchemy
 
-def write_to_database(pandas_dataframe, database_table):
+def write_to_database(data, name):
     try:
-    # GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-        engine = db_connection.database_connection()
-        pandas_dataframe.to_sql(os.getenv(database_table), engine, if_exists='append', index=False)
+        data.to_sql(os.getenv(name), database_connection(), if_exists='append', index=False)
         print("Data was inserted into the database")
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
