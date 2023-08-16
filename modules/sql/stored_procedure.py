30d4af3fa25f2b4b35d3c32f0e5bd8b21f812ab5
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-16 16:19:37 +0100

    Added functionality to allow the stored procedure be called by the script

--- modules/sql/stored_procedure.py
+++ modules/sql/stored_procedure.py
@@ -1,11 +1,14 @@
-import os
 from dotenv import load_dotenv
 load_dotenv(".env")
-import pandas as pd
 from connect import database_connection
 import sqlalchemy as sqlalchemy
 
-def call_stored_function(name):
+
+def call_stored_procedure():
     try:
+        cursor = database_connection().raw_connection().cursor();
     except Exception as ex:
-print("Connection could not be made due to the following error: \n", ex)
+        print("Connection could not be made due to the following error: \n", ex)
+
+
+call_stored_procedure();
