fc205f6301759a74cb22962a2991a3424c3f52fd
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-16 12:22:51 +0100

    Moved modules that process xml files to didicated directory

--- modules/xml/xml1.py
+++ modules/xml/xml1.py
@@ -5,10 +5,15 @@ load_dotenv(".env")
 
 bom = os.getenv('XML')
 
-# The loop new extracts the components from the xml file
-with open(p) as fd:
+# The output is formatted to be more readable for the end user
+
+with open(b) as fd:
     xml_dict = xmltodict.parse(fd.read(), process_namespaces=True)
     for product in xml_dict['BOMRecords']['BOMRecord']:
         product_code = product['Reference']
         product_description = product['Description']
-        print( product_code + ' - ' + product_description )
+        # print( product_code + ' - ' + product_description )
+        print("================================================================================================")
+        print(product_code + " " + product_description)
+
+
