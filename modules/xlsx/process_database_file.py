43aa7ec0b1c2333c6808239e3486baff9fb6648f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-17 12:40:37 +0100

    Corrected Errors in data processing

--- modules/xlsx/process_database_file.py
+++ modules/xlsx/process_database_file.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import sqlalchemy as sqlalchemy
-from ..db.write import write_to_database
 import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
@@ -8,6 +7,7 @@ load_dotenv("../../.env")
 dataframe = pd.read_excel(os.getenv('DATABASE_FILE'))
 
 database_data = dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
-new_data = sage_column_data.rename(columns={os.getenv('Column1'):os.getenv('Column_1'),os.getenv('Column2'):os.getenv('Column_2'),os.getenv('Column3'):os.getenv('Column_3'),os.getenv('Column4'):os.getenv('Column_4'),})
+new_data = database_data.rename(columns={os.getenv('Column1'):os.getenv('Column_1'),os.getenv('Column2'):os.getenv('Column_2'),os.getenv('Column3'):os.getenv('Column_3'),os.getenv('Column4'):os.getenv('Column_4'),})
 
-write_to_database(new_data, 'tpdb_table')
+print(new_data);
+
