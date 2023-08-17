fd4a1629900810a9affb70bcd5afa0c9a09a11ac
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-17 13:02:01 +0100

    Corrected the imports for the database connection module

--- modules/xlsx/process_database_file.py
+++ modules/xlsx/process_database_file.py
@@ -2,12 +2,12 @@ import pandas as pd
 import sqlalchemy as sqlalchemy
 import os
 from dotenv import load_dotenv
-load_dotenv("../../.env")
+load_dotenv("../.env")
+from db.write import write_to_database
 
 dataframe = pd.read_excel(os.getenv('DATABASE_FILE'))
 
 database_data = dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
 new_data = database_data.rename(columns={os.getenv('Column1'):os.getenv('Column_1'),os.getenv('Column2'):os.getenv('Column_2'),os.getenv('Column3'):os.getenv('Column_3'),os.getenv('Column4'):os.getenv('Column_4'),})
 
-print(new_data);
-
+write_to_database(new_data, 'tpdb_table')
