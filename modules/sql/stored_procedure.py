c86ff5eaa6c2d289157c95cd09c2988eb0956026
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-17 11:29:37 +0100

    Added functionality to create cursor and execute stored procedure

--- modules/sql/stored_procedure.py
+++ modules/sql/stored_procedure.py
@@ -3,6 +3,8 @@ from connect import database_connection as db
 def call_stored_procedure():
     try:
         connection = db().raw_connection()
+        cursor = connection.cursor()
+        cursor.execute('Call stored_procedure();');
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
