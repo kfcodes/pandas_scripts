689a05a624429364bb9b479e5eb5d1c35942f53d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-17 12:14:21 +0100

    Added functionality to read the data from the file and process using pandas

--- modules/xlsx/process_database_file.py
+++ modules/xlsx/process_database_file.py
@@ -1,8 +1,13 @@
 import pandas as pd
+import sqlalchemy as sqlalchemy
 import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
 
 database_output_file = os.getenv('DATABASE_FILE')
 dataframe = pd.read_excel(database_output_file)
+
+database_data = dataframe.loc[:, [ os.getenv('Column1'), os.getenv('Column2'), os.getenv('Column3'), os.getenv('Column4')]]
+
+correct_column_names = sage_column_data.rename(columns={os.getenv('Column1'):os.getenv('Column_1'),os.getenv('Column2'):os.getenv('Column_2'),os.getenv('Column3'):os.getenv('Column_3'),os.getenv('Column4'):os.getenv('Column_4'),})
 
