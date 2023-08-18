03babd37991cdc167a3370350b1714f95d0424f8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 14:14:56 +0100

    Added the update poi function to po file processor controller

--- modules/po_file.py
+++ modules/po_file.py
@@ -1,7 +1,8 @@
 from xlsx.read_database_file import read_xlsx_file
 from process_data.process_po_data import process_data
-from database.index import update_po
+from database.index import update_po, update_poi
 
-data= read_xlsx_file('PO_FILE')
-formatted_data = process_data(data);
-update_po(formatted_data);
+inputdata= read_xlsx_file('PO_FILE')
+data = process_data(inputdata);
+update_po(data[0]);
+update_poi(data[1]);
