828f337e2d322ced4641aa8ce6f22d47eb123ae8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 12:51:13 +0100

    Added function to add po data to DB

--- modules/database/index.py
+++ modules/database/index.py
@@ -1,7 +1,7 @@
 from .stored_procedure import call_stored_procedure
 from .write import write_to_database
 
-def updateDB(data):
+def update_db(data):
     try:
         call_stored_procedure("STOREDPROCEDURE1");
         write_to_database(data, 'tpdb_table')
@@ -9,18 +9,10 @@ def updateDB(data):
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
-# def updatePo(data):
-#     try:
-#         call_stored_procedure("STOREDPROCEDURE3");
-#         write_to_database(data, 'tpo_table')
-#         call_stored_procedure("STOREDPROCEDURE4");
-#     except Exception as ex:
-#         print("Connection could not be made due to the following error: \n", ex)
-#
-# def updatePoi(data):
-#     try:
-#         call_stored_procedure("STOREDPROCEDURE5");
-#         write_to_database(data, 'tpoi_table')
-#         call_stored_procedure("STOREDPROCEDURE6");
-#     except Exception as ex:
-#         print("Connection could not be made due to the following error: \n", ex)
+def update_po(data):
+    try:
+        call_stored_procedure("STOREDPROCEDURE3");
+        write_to_database(data, 'tpo_table')
+        call_stored_procedure("STOREDPROCEDURE4");
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
