89b0a996834333eef7c10868fc1197c05824189c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 14:26:24 +0100

    Added test code the process po data module to test functionality

--- modules/process_data/process_po_data.py
+++ modules/process_data/process_po_data.py
@@ -5,6 +5,10 @@ load_dotenv("../.env")
 
 def process_data(data):
     try:
+        print(data)
+        test="test"
+        test2="test2"
+        return(test, test2);
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
