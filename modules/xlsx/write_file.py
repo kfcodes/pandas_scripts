19edfb6edf4b48da37464f05482486565721b881
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 15:04:11 +0100

    Created a module to write the data to an excel file

---
+++ modules/xlsx/write_file.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv("../.env")
+
+def write_xlsx_file(data):
+    try:
+        data.to_excel("test.xlsx")
+        print("Data was written to file")
+    except Exception as ex:
+        print("Could not write to file file: \n", ex)
+
