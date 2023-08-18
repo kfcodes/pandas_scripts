e195768c921c53525cce1ce904c2c7eaa0ea5d2a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 15:26:06 +0100

    Improved module to read pl data

--- modules/read_pl.py
+++ modules/read_pl.py
@@ -1,18 +1,11 @@
 import pandas as pd
-import sqlalchemy as sqlalchemy
-from sql import write_pandas_dataframe_to_sql
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, insert, select
 
-metadata = MetaData()
-
-product_database_dataframe = pd.ExcelFile(os.getenv('PACKING_LIST'))
-
-def get_pallet_data():
+def get_pl_data():
+    product_database_dataframe = pd.ExcelFile(os.getenv('PL'))
     for sheet in product_database_dataframe.sheet_names:
         sheet_data = product_database_dataframe.parse(sheet, usecols="f:g,h")
         noNa = sheet_data.dropna(axis=0, how = 'all')
-        print("this is the pallet data")
         print(noNa)
