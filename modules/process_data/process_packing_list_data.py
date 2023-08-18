71d2b231d890d76e560a7cac675b7f949a284d27
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 15:05:16 +0100

    Created a module to process the packing list data

---
+++ modules/process_data/process_packing_list_data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from dotenv import load_dotenv
+load_dotenv("../.env")
+
+def process_data(data):
+    try:
+        df = pd.json_normalize(data)
+        return(df)
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
