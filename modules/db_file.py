eb1998d9fca511ab904105f4cddfa57f20084e4d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-18 14:13:41 +0100

    Corrected the imported function for the db_file controller

--- modules/db_file.py
+++ modules/db_file.py
@@ -1,7 +1,7 @@
 from xlsx.read_database_file import read_xlsx_file
 from process_data.process_database_data import process_data
-from database.index import updateDB
+from database.index import update_db
 
 data= read_xlsx_file('DATABASE_FILE')
 formatted_data = process_data(data);
-updateDB(formatted_data);
+update_db(formatted_data);
