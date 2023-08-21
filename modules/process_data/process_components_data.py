f1148f9dd46a065507691519d532ed54af1d88f4
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-21 16:10:45 +0100

    Created the outline of the module for processing the component data

---
+++ modules/process_data/process_components_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv("../.env")
+
+def process_component_data(data):
+    try:
+
+        return(processed_data)
+
+    except Exception as ex:
+
+        print("Data could not be processed: \n", ex)
+
+
