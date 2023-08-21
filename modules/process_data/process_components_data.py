80e841c6054a08e3c295db4b698ecd629956d8a1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-21 16:17:38 +0100

    Added code to the module to process the component data

--- modules/process_data/process_components_data.py
+++ modules/process_data/process_components_data.py
@@ -6,6 +6,10 @@ load_dotenv("../.env")
 def process_component_data(data):
     try:
 
+        component_data = data.loc[:, [ os.getenv('COMPONENTINPUT1'), os.getenv('COMPONENTINPUT2'), os.getenv('COMPONENTINPUT3')]]
+
+        processed_data = component_data.rename(columns={os.getenv('COMPONENTINPUT1'):os.getenv('COMPONENTOUTPUT1'),os.getenv('COMPONENTINPUT2'):os.getenv('COMPONENTOUTPUT2'),os.getenv('COMPONENTINPUT3'):os.getenv('COMPONENTOUTPUT3'),})
+
         return(processed_data)
 
     except Exception as ex:
