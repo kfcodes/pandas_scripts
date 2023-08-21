3d2b3bb9419788d4148af1376cea34718daffc3e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-21 15:31:35 +0100

    Added better formatting to db process module

--- modules/process_data/process_database_data.py
+++ modules/process_data/process_database_data.py
@@ -5,10 +5,16 @@ load_dotenv("../.env")
 
 def process_db_data(data):
     try:
+
         database_data = data.loc[:, [ os.getenv('DBINPUT1'), os.getenv('DBINPUT2'), os.getenv('DBINPUT3'), os.getenv('DBINPUT4')]]
-        new_data = database_data.rename(columns={os.getenv('DBINPUT1'):os.getenv('DBOUTPUT1'),os.getenv('DBINPUT2'):os.getenv('DBOUTPUT2'),os.getenv('DBINPUT3'):os.getenv('DBOUTPUT3'),os.getenv('DBINPUT4'):os.getenv('DBOUTPUT4'),})
-        return(new_data)
+
+
+        processed_data = database_data.rename(columns={os.getenv('DBINPUT1'):os.getenv('DBOUTPUT1'),os.getenv('DBINPUT2'):os.getenv('DBOUTPUT2'),os.getenv('DBINPUT3'):os.getenv('DBOUTPUT3'),os.getenv('DBINPUT4'):os.getenv('DBOUTPUT4'),})
+
+        return(processed_data)
+
     except Exception as ex:
+
         print("Data could not be processed: \n", ex)
 
 
