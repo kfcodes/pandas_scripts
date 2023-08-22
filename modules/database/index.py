de191ac586fb8a8681a7d62e94320d7395d820b2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-22 11:57:25 +0100

    Created function to update the labels in the DB

--- modules/database/index.py
+++ modules/database/index.py
@@ -32,3 +32,11 @@ def update_components(data):
         call_stored_procedure("STOREDPROCEDURE8");
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
+
+def update_labels(data):
+    try:
+        call_stored_procedure("STOREDPROCEDURE9");
+        write_to_database(data, 'LABELS')
+        call_stored_procedure("STOREDPROCEDURE10");
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
