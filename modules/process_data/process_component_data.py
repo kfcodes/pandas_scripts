e89f0adcde91f435ec77543c5971cafe51fe8afb
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-24 17:56:01 +0100

    Removed test code and unwanted rows from component data table

--- modules/process_data/process_component_data.py
+++ modules/process_data/process_component_data.py
@@ -8,27 +8,12 @@ def process_component_data(data):
 
         component_data = data.loc[:, [ os.getenv('COMPONENTINPUT1'), os.getenv('COMPONENTINPUT2'), os.getenv('COMPONENTINPUT3')]]
 
-        # component_data = data.loc[:, [os.getenv('COMPONENTINPUT1')]]
-
-        # processed_data = component_data.rename(columns={os.getenv('COMPONENTINPUT1'):os.getenv('COMPONENTOUTPUT1'),})
-
         processed_data = component_data.rename(columns={os.getenv('COMPONENTINPUT1'):os.getenv('COMPONENTOUTPUT1'),os.getenv('COMPONENTINPUT2'):os.getenv('COMPONENTOUTPUT2'),os.getenv('COMPONENTINPUT3'):os.getenv('COMPONENTOUTPUT3'),})
 
         processed_data = processed_data[~processed_data[os.getenv('COMPONENTOUTPUT1')].str.contains(os.getenv('COMPONENTDELETE1'))]
         processed_data = processed_data[~processed_data[os.getenv('COMPONENTOUTPUT1')].str.contains(os.getenv('COMPONENTDELETE2'))]
 
-        # processed_data[os.getenv('COMPONENTOUTPUT1')] = processed_data[os.getenv('COMPONENTOUTPUT1')].astype('string')
-        # processed_data[os.getenv('COMPONENTOUTPUT2')] = processed_data[os.getenv('COMPONENTOUTPUT2')].astype('string')
-
-        # processed_data[os.getenv('COMPONENTOUTPUT3')] = pd.to_numeric(processed_data[os.getenv('COMPONENTOUTPUT3')])
-
-        processed_data.to_csv("output.csv",index=False, header=False, quotechar='"', encoding='utf-8', quoting=csv.QUOTE_ALL)
-        #
-        processed_data.dtypes()
-
 
         return(processed_data)
     except Exception as ex:
         print("Data could not be processed: \n", ex)
-
-
