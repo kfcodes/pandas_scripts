9a8335d45e01ce15b35c744acda105275335996d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-24 18:23:27 +0100

    Testing module to process po data

--- modules/process_data/process_po_data.py
+++ modules/process_data/process_po_data.py
@@ -6,18 +6,29 @@ load_dotenv("../.env")
 def process_po_data(data, po_name_data):
     try:
 
-        po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3')]]
-        name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),os.getenv('POINPUT3'):os.getenv('POOUTPUT3'),})
+        # po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3')]]
+        po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2')]]
+
+        # name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),os.getenv('POINPUT3'):os.getenv('POOUTPUT3'),})
+        name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),})
+
         name_table.dropna();
-        name_table[os.getenv('POINPUT1')].drop_duplicates().reset_index(drop=True)
+        name_table[os.getenv('POOUTPUT1')].drop_duplicates().reset_index(drop=True)
+
+        # print(name_table);
+
         po_data = name_table;
+        #
+        # po_items = data.loc[:, [ os.getenv('POINPUT4'), os.getenv('POINPUT5'), os.getenv('POINPUT6'), os.getenv('POINPUT7')]]
+        #
+        # data_table = po_items.rename(columns={os.getenv('POINPUT4'):os.getenv('POOUTPUT4'),os.getenv('POINPUT5'):os.getenv('POOUTPUT5'),os.getenv('POINPUT6'):os.getenv('POOUTPUT6'),os.getenv('POINPUT7'):os.getenv('POOUTPUT7'),})
+        #
+        # data_table.dropna();
+        #
+        # po_item_data = data_table;
+        #
+        # return(po_data, po_item_data);
 
-        po_items = data.loc[:, [ os.getenv('POINPUT4'), os.getenv('POINPUT5'), os.getenv('POINPUT6'), os.getenv('POINPUT7')]]
-        data_table = po_items.rename(columns={os.getenv('POINPUT4'):os.getenv('POOUTPUT4'),os.getenv('POINPUT5'):os.getenv('POOUTPUT5'),os.getenv('POINPUT6'):os.getenv('POOUTPUT6'),os.getenv('POINPUT7'):os.getenv('POOUTPUT7'),})
-        data_table.dropna();
-        po_item_data = data_table;
-
-        return(po_data, po_item_data);
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
