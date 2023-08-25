814d952fce53601f2575ec591e8fa1dcc9ec722a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-25 14:08:34 +0100

    Module processes and removes rows that are not required

--- modules/process_data/process_po_data.py
+++ modules/process_data/process_po_data.py
@@ -6,34 +6,29 @@ load_dotenv("../.env")
 def process_po_data(data, po_name_data):
     try:
 
-        po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3'), os.getenv('POINPUT4')]]
+        # po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3'), os.getenv('POINPUT4')]]
 
-        # Select Only the rows we want
-        po_names = po_names[po_names[os.getenv('POINPUT4')].str.contains(os.getenv('PODELETE1'))]
-        po_names.drop(columns=[os.getenv('POINPUT4')])
-        po_names = po_names[~po_names[os.getenv('POINPUT1')].str.contains(os.getenv('PODELETE2'))]
-        po_names = po_names[~po_names[os.getenv('POINPUT1')].str.contains(os.getenv('PODELETE3'))]
+        # po_names = po_names[po_names[os.getenv('POINPUT4')].str.contains(os.getenv('PODELETE1'))]
+        # po_names.drop(columns=[os.getenv('POINPUT4')])
+        # po_names = po_names[~po_names[os.getenv('POINPUT1')].str.contains(os.getenv('PODELETE2'))]
+        # po_names = po_names[~po_names[os.getenv('POINPUT1')].str.contains(os.getenv('PODELETE3'))]
 
-        name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),os.getenv('POINPUT3'):os.getenv('POOUTPUT3'),})
-        name_table.dropna();
-        name_table[os.getenv('POOUTPUT1')].drop_duplicates().reset_index(drop=True)
+        # name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),os.getenv('POINPUT3'):os.getenv('POOUTPUT3'),})
+        # name_table.dropna();
+        # name_table[os.getenv('POOUTPUT1')].drop_duplicates().reset_index(drop=True)
 
-        po_data = name_table[[ os.getenv('POOUTPUT1'), os.getenv('POOUTPUT2'), os.getenv('POOUTPUT3') ]]
+        # po_data = name_table[[ os.getenv('POOUTPUT1'), os.getenv('POOUTPUT2'), os.getenv('POOUTPUT3') ]]
+
+        po = data.loc[:,[os.getenv('POINPUT5'), os.getenv('POINPUT6'), os.getenv('POINPUT7'), os.getenv('POINPUT8')]]
+
+        po.dropna();
+        po = po[~po[os.getenv('POINPUT8')].str.contains(os.getenv('PODELETE5'))]
+        po = po[~po[os.getenv('POINPUT5')].str.contains(os.getenv('PODELETE2'))]
+        po = po[~po[os.getenv('POINPUT5')].str.contains(os.getenv('PODELETE3'))]
+        po = po[~po[os.getenv('POINPUT6')].str.contains(os.getenv('PODELETE4'), na=False)]
 
 
-        po_details = data.loc[:,[os.getenv('POINPUT5'), os.getenv('POINPUT6'), os.getenv('POINPUT7')]]
-
-        data_table = po_details.rename(columns={os.getenv('POINPUT5'):os.getenv('POOUTPUT5'), os.getenv('POINPUT6'):os.getenv('POOUTPUT6'), os.getenv('POINPUT7'):os.getenv('POOUTPUT7'),})
-
-        data_table = data_table[~data_table[os.getenv('POOUTPUT6')].str.contains(os.getenv('POIDELETE1'))]
-        data_table.dropna();
-
-        po_item_data = data_table;
-        
-        print(po_item_data);
-
-        # return(po_data, po_item_data);
-
+        return(po);
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
