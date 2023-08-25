bd6c79c020d95f6cb8fbb7c7ebd85d9a7f449d74
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-25 11:49:30 +0100

    Reading the excel file removing the first row

--- modules/po_file.py
+++ modules/po_file.py
@@ -1,8 +1,9 @@
 from xlsx.read_database_file import read_xlsx_file
+from xlsx.read_database_file import read_xlsx_file_remove_first_row
 from process_data.process_po_data import process_po_data
 from database.index import update_po, update_poi
 
-inputdata = read_xlsx_file('POFILE')
+inputdata = read_xlsx_file_remove_first_row('POFILE')
 po_name_data = read_xlsx_file('PONAMEFILE')
 process_po_data(inputdata,po_name_data);
 # data = process_po_data(inputdata, po_name_data);
