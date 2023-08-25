0da4fd8492137300a1eec40abef561c943a586fb
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-25 11:22:25 +0100

    The function to process the data has acces to the data from both files

--- modules/po_file.py
+++ modules/po_file.py
@@ -2,11 +2,10 @@ from xlsx.read_database_file import read_xlsx_file
 from process_data.process_po_data import process_po_data
 from database.index import update_po, update_poi
 
-# inputdata = read_xlsx_file('POFILE')
+inputdata = read_xlsx_file('POFILE')
 po_name_data = read_xlsx_file('PONAMEFILE')
-# process_po_data(po_name_data);
-data = process_po_data(po_name_data);
+process_po_data(inputdata,po_name_data);
 # data = process_po_data(inputdata, po_name_data);
-update_po(data);
+# update_po(data);
 # update_po(data[0]);
 # update_poi(data[1]);
