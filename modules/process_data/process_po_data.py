a3dc8db0eab2cd9073447a17d05acf798e711691
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-25 09:55:55 +0100

    Added functionality to delete rows from po data the we do not require

--- modules/process_data/process_po_data.py
+++ modules/process_data/process_po_data.py
@@ -3,14 +3,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
-def process_po_data(data, po_name_data):
+# def process_po_data(data, po_name_data):
+def process_po_data(po_name_data):
     try:
 
-        # po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3')]]
+        # po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2'), os.getenv('POINPUT3'), os.getenv('POINPUT4')]]
         po_names = po_name_data.loc[:, [ os.getenv('POINPUT1'), os.getenv('POINPUT2')]]
+
+        # Select Only the rows we want
+        # po_names = po_names[po_names[os.getenv('POINPUT4')].str.contains(os.getenv('PODELETE1'))]
 
         # name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),os.getenv('POINPUT3'):os.getenv('POOUTPUT3'),})
         name_table = po_names.rename(columns={os.getenv('POINPUT1'):os.getenv('POOUTPUT1'),os.getenv('POINPUT2'):os.getenv('POOUTPUT2'),})
+
+        name_table = name_table[~name_table[os.getenv('POOUTPUT1')].str.contains(os.getenv('PODELETE2'))]
 
         name_table.dropna();
         name_table[os.getenv('POOUTPUT1')].drop_duplicates().reset_index(drop=True)
@@ -18,6 +24,7 @@ def process_po_data(data, po_name_data):
         po_data = name_table;
 
         print(po_data)
+
         #
         # po_items = data.loc[:, [ os.getenv('POINPUT4'), os.getenv('POINPUT5'), os.getenv('POINPUT6'), os.getenv('POINPUT7')]]
         #
