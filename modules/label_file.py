499f4955a86a1350ca8be42177a44d4a8163e0e4
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-08-27 19:59:21 +0100

    Added value for the sheet name to be used to be an environment variable

--- modules/label_file.py
+++ modules/label_file.py
@@ -1,7 +1,10 @@
-from xlsx.read_database_file import read_xlsx_file
+from xlsx.read_database_file import read_xlsx_file_by_sheet
 from process_data.process_label_data import process_label_data
 from database.index import update_labels
+import os
+from dotenv import load_dotenv
+load_dotenv("../.env")
 
-data = read_xlsx_file('LABELSFILE')
+data = read_xlsx_file_by_sheet('LABELSFILE', os.getenv('SHEETNAME'))
 processed_data = process_label_data(data);
 update_labels(processed_data);
