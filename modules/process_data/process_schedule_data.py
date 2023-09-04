00ee534ba0ab1109c20ff63efb8a7a41398088b3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-04 16:15:33 +0100

    Added functionality to process the data from the schedule file

--- modules/process_data/process_schedule_data.py
+++ modules/process_data/process_schedule_data.py
@@ -3,9 +3,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
-def process_schedule_data():
+def process_schedule_data(data):
     try:
-        return("processed_data")
+
+        sheet_data = data.loc[:, [ os.getenv('SCHEDULEINPUT1'), os.getenv('SCHEDULEINPUT2'), os.getenv('SCHEDULEINPUT3'), os.getenv('SCHEDULEINPUT4')]]
+
+        processed_data = sheet_data.rename(columns={os.getenv('SCHEDULEINPUT1'):os.getenv('SCHEDULEOUTPUT1'),os.getenv('SCHEDULEINPUT2'):os.getenv('SCHEDULEOUTPUT2'),os.getenv('SCHEDULEINPUT3'):os.getenv('SCHEDULEOUTPUT3'),os.getenv('SCHEDULEINPUT4'):os.getenv('SCHEDULEOUTPUT4'),})
+
+        return(processed_data)
 
     except Exception as ex:
 
