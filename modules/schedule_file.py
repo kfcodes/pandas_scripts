ffa2637be774a4a576710392647aa80641265749
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-04 16:02:51 +0100

    Created a module to process schedule file

---
+++ modules/schedule_file.py
@@ -0,0 +1,7 @@
+from xlsx.read_database_file import read_xlsx_file
+from process_data.process_schedule_data import process_schedule_data
+from database.index import update_schedule
+
+data= read_xlsx_file('SCHEDULEFILE')
+processed_data = process_schedule_data(data);
+update_schedule(processed_data);
