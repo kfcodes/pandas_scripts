f27113e662aa3b95428c1681308f50fd5787724b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 12:35:54 +0100

    Moved the production info to a function with a try catch block that takes in the ID value and prints the PDF

--- modules/production_info.py
+++ modules/production_info.py
@@ -2,9 +2,16 @@ from database.read_db_index import get_production_documentation
 from pdf.create_pdf import create_pdf
 from print.print_pdf import print_pdf
 
-# Get the info from the DB
-data = get_production_documentation(id);
-# Create the PDF file
-pdf = create_pdf(data);
-# Print the PDF file
-print_pdf(pdf);
+
+def print_production_pdf(id):
+    try:
+
+        # Get the info from the DB
+        data = get_production_documentation(id);
+        # Create the PDF file
+        pdf = create_pdf(data);
+        # Print the PDF file
+        print_pdf(pdf);
+
+    except Exception as ex:
+        print("Could not print the PDF file due to the following: \n", ex)
