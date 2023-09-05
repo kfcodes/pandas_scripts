29b6d6742b02705bdc339fe8298e1f6b5da47eb4
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 11:51:43 +0100

    Created a module to print the production info with DB data

---
+++ modules/production_info.py
@@ -0,0 +1,8 @@
+from database.index import get_production_data
+
+# Get the info from the DB
+production_in(id);
+# Create the PDF file
+pdf = create_pdf_file(data);
+# Print the PDF file
+print_pdf(pdf);
