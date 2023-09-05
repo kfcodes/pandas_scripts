9c1aefc2e26750f11285b582f278123e1df64f09
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 14:47:13 +0100

    Added modularity to function to print PDF files

--- modules/print/print_pdf.py
+++ modules/print/print_pdf.py
@@ -1,9 +1,18 @@
+import cups
 import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
 
-def print_pdf():
-	try:
+def print_A4_page(file_path, printer_name):
 
-	except Exception as ex:
-		print("Could not print pdf file due to the following: \n", ex)
+    conn = cups. Connection()
+    printers = conn.getPrinters()
+    
+    if printer_name in printers:
+        try:
+            conn.printFile(printer_name, file_path, "Print Job", {})
+            print("File sent to printer successfully.")
+        except cups.IPPError as e:
+            print(f"Error printing file: {e}")
+    else:
+        print(f"Printer '{printer_name}' not found.")
