4cbfa1cc3021f439996fda312522207bfd2c7ba8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 15:27:56 +0100

    Module takes the first arguement as the ID value

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,9 +1,9 @@
+import sys
 from database.read_db_index import get_production_documentation
 from pdf.create_pdf import create_pdf
 from print.print_pdf import print_a4_pdf
 
-
-id = input("Enter id:")
+id = int(sys.argv[1])
 data = get_production_documentation(id);
 pdf_file = create_pdf(data);
-print_a4_pdf(pdf_file)
+# print_a4_pdf(pdf_file)
