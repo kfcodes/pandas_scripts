0d361cdda2fd58822f9c32b37f020bb2f6ea61fe
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 15:53:18 +0100

    Removed code for testing

--- modules/print/print_zpl.py
+++ modules/print/print_zpl.py
@@ -6,12 +6,15 @@ mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 def print_large_label():
 	try:
-
 		host = os.getenv("BIGLABELID1") 
 		port = os.getenv("BIGLABELPORT1") 
-		mysocket.connect((host, port)) #connecting to host
-		mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ")#using bytes
-		mysocket.close () #closing connection
+		# mysocket.connect((host, port)) #connecting to host
+		# mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ")#using bytes
+		# mysocket.close () #closing connection
+		print(f"Printed the Label to {host}, {port}")
 
 	except Exception as ex:
 		print("Could not print label due to the following: \n", ex)
+
+
+print_large_label()
