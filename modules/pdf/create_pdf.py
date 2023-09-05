b08a14f813aae0a91edb522d8e25185ead3400f8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 11:45:03 +0100

    Moved the create PDF module to a try catch block

--- modules/pdf/create_pdf.py
+++ modules/pdf/create_pdf.py
@@ -1,9 +1,17 @@
 from fpdf import FPDF
+import os
+from dotenv import load_dotenv
+load_dotenv("../.env")
 
-pdf = FPDF()
-pdf.add_page()
+def create_pdf():
+    try:
+        pdf = FPDF()
+        pdf.add_page()
 
-pdf.set_font('Arial', 'B', 16)
-pdf.cell(40, 10, 'Hello World!')
+        pdf.set_font('Arial', 'B', 16)
+        pdf.cell(40, 10, 'Hello World!')
 
-pdf.output('test.pdf', 'F')
+        pdf.output('test.pdf', 'F')
+        return('test.pdf')
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
