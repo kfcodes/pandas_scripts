3522c6539eea4e4fb8c3dd5ddddf3fbcf6f2c06c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 14:54:41 +0100

    Improved the module for printing the production information

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,17 +1,15 @@
 from database.read_db_index import get_production_documentation
 from pdf.create_pdf import create_pdf
-from print.print_pdf import print_pdf
-
+from print.print_pdf import print_a4_pdf
 
 def print_production_pdf(id):
     try:
 
-        # Get the info from the DB
         data = get_production_documentation(id);
-        # Create the PDF file
-        pdf = create_pdf(data);
-        # Print the PDF file
-        print_pdf(pdf);
+
+        pdf_file = create_pdf(data);
+
+        print_a4_pdf(pdf_file)
 
     except Exception as ex:
         print("Could not print the PDF file due to the following: \n", ex)
