e22b2b84f113b9cf3dd6ae2b9af48da964188edc
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 15:22:48 +0100

    Removed the try catch block for simplicity

--- modules/production_info.py
+++ modules/production_info.py
@@ -2,14 +2,8 @@ from database.read_db_index import get_production_documentation
 from pdf.create_pdf import create_pdf
 from print.print_pdf import print_a4_pdf
 
-def print_production_pdf(id):
-    try:
 
-        data = get_production_documentation(id);
-
-        pdf_file = create_pdf(data);
-
-        print_a4_pdf(pdf_file)
-
-    except Exception as ex:
-        print("Could not print the PDF file due to the following: \n", ex)
+id = input("Enter id:")
+data = get_production_documentation(id);
+pdf_file = create_pdf(data);
+print_a4_pdf(pdf_file)
