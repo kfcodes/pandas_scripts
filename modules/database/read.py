0f1f11203f20d5e4f16e6dc4e19b49882e609cd6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-05 12:15:30 +0100

    Added module to read selected data from the database using selection and id

--- modules/database/read.py
+++ modules/database/read.py
@@ -6,7 +6,21 @@ from .connect import database_connection
 
 def read_to_dataframe(name):
     try:
+
         data = pd.read_sql(os.getenv(name),database_connection())
         return(data)
+
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
+def read_select_to_dataframe(SELECTION, id):
+    try:
+
+        selection = os.getenv(SELECTION)+id
+
+        data = pd.read_sql(selection ,database_connection())
+
+        return(data)
+
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
