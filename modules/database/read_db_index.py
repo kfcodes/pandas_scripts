6641612f93e1a3878108f66b8b444df7a9a720e0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-06 11:24:18 +0100

    Added function to retrieve product components from DB

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -4,9 +4,22 @@ def get_production_documentation(id):
     try:
 
         select = "PRODUCTIONINFO"
-        data = read_select_to_dataframe(select, id)
+        finished_product_info = read_select_to_dataframe(select, id)
 
-        return(data);
+        return(finished_product_info);
+
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
+
+def get_product_components(id):
+    try:
+
+        select = "COMPONENTS"
+
+        components = read_select_to_dataframe(select, id)
+
+        return(components);
 
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
