11d0104dd740956c37bfc6ddcd151ffccd5d4265
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-07 12:05:11 +0100

    Commented out code for testing new module

--- modules/schedule_file.py
+++ modules/schedule_file.py
@@ -1,7 +1,9 @@
-from xlsx.read_database_file import read_xlsx_file
+from xlsx.read_xlsx_file import read_xlsx_file_skip_first_row
 from process_data.process_schedule_data import process_schedule_data
 from database.write_db_index import update_schedule
 
-data= read_xlsx_file('SCHEDULEFILE')
-processed_data = process_schedule_data(data);
-update_schedule(processed_data);
+data= read_xlsx_file_skip_first_row('SCHEDULEFILE')
+# print(data.columns)
+process_schedule_data(data);
+# processed_data = process_schedule_data(data);
+# update_schedule(processed_data);
