8d9c0900538df928c76b173ee1f26f9748fc1041
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-07 10:53:42 +0100

    Added functions to take in several columns of data from schedule sheet

--- modules/process_data/process_schedule_data.py
+++ modules/process_data/process_schedule_data.py
@@ -6,7 +6,14 @@ load_dotenv("../.env")
 def process_schedule_data(data):
     try:
 
-        sheet_data = data.loc[:, [ os.getenv('SCHEDULEINPUT1'), os.getenv('SCHEDULEINPUT2'), os.getenv('SCHEDULEINPUT3'), os.getenv('SCHEDULEINPUT4')]]
+        sheet_data_part_1 = data.loc[:, [ os.getenv('SCHEDULEINPUT1'), os.getenv('SCHEDULEINPUT2'), os.getenv('SCHEDULEINPUT3')]]
+
+        sheet_data_part_2 = data.loc[:, [ os.getenv('SCHEDULEINPUT6'), os.getenv('SCHEDULEINPUT7'), os.getenv('SCHEDULEINPUT8')]]
+
+        sheet_data_part_3 = data.loc[:, [ os.getenv('SCHEDULEINPUT11'), os.getenv('SCHEDULEINPUT12'), os.getenv('SCHEDULEINPUT13')]]
+        
+        sheet_data_part_4 = data.loc[:, [ os.getenv('SCHEDULEINPUT16'), os.getenv('SCHEDULEINPUT17'), os.getenv('SCHEDULEINPUT18')]]
+
 
         processed_data = sheet_data.rename(columns={os.getenv('SCHEDULEINPUT1'):os.getenv('SCHEDULEOUTPUT1'),os.getenv('SCHEDULEINPUT2'):os.getenv('SCHEDULEOUTPUT2'),os.getenv('SCHEDULEINPUT3'):os.getenv('SCHEDULEOUTPUT3'),os.getenv('SCHEDULEINPUT4'):os.getenv('SCHEDULEOUTPUT4'),})
 
