2528125c1521fc61b03b59e6e6b17e353d5038e3
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 16:12:32 +0100

    updated the function for reading the database index file to use environment variables

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -1,13 +1,15 @@
-from .read import read_select_to_dataframe
+import os
+from dotenv import load_dotenv
+load_dotenv("../../.env")
+import pandas as pd
+from .connect import database_connection
 
-def get_production_documentation(id):
+def get_production_documentation(selection):
     try:
-        selection = "PRODUCTIONINFO"
-        finished_product_info = read_select_to_dataframe(selection, id)
+        finished_product_info = pd.read_sql(selection ,database_connection())
         return(finished_product_info);
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
-
 
 def get_product_components(id):
     try:
