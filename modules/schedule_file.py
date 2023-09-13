472a5f38306187fc557cb67cd61837878cc2469d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 15:59:46 +0100

    Updated the schedule file to enter the processed data into the DB

--- modules/schedule_file.py
+++ modules/schedule_file.py
@@ -2,8 +2,6 @@ from xlsx.read_xlsx_file import read_xlsx_file_sheet_skip_first_row
 from process_data.process_schedule_data import process_schedule_data
 from database.write_db_index import update_schedule
 
-
 data= read_xlsx_file_sheet_skip_first_row('SCHEDULEFILE', sheet)
 processed_data = process_schedule_data(data);
-print(processed_data);
-# update_schedule(processed_data);
+update_schedule(processed_data);
