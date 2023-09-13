ca207bb1a93223fa5704f4b08280da8cf6a1300e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 16:02:58 +0100

    Added functionality to pass id and environment variables when retrieving information from DB

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,10 +1,10 @@
-import sys
+# import sys
+import os
+from dotenv import load_dotenv
+load_dotenv("../.env")
+import pandas as pd
 from database.read_db_index import get_production_documentation, get_product_components
-from pdf.create_production_info_pdf import create_production_info_pdf
-from print.print_pdf import print_a4_pdf
 
-id = str(sys.argv[1])
-info = get_production_documentation(id);
-components = get_product_components(id);
-pdf_file = create_production_info_pdf(info, components);
-# print_a4_pdf(pdf_file)
+id = "test"
+selection = f"{os.getenv('PRODUCTIONINFO')}{id}"
+get_production_documentation(selection)
