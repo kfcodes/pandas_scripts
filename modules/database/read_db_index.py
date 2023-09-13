cca2478378f4b29362c9b2e1560ae75148b78e59
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 16:15:22 +0100

    Removed unused code and streamlined how to retrieve data from the DB

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -4,17 +4,9 @@ load_dotenv("../../.env")
 import pandas as pd
 from .connect import database_connection
 
-def get_production_documentation(selection):
+def get_database_info(selection):
     try:
-        finished_product_info = pd.read_sql(selection ,database_connection())
-        return(finished_product_info);
-    except Exception as ex:
-        print("Connection could not be made due to the following error: \n", ex)
-
-def get_product_components(id):
-    try:
-        selection = "COMPONENTS"
-        components = read_select_to_dataframe(selection, id)
-        return(components);
+        info = pd.read_sql(selection ,database_connection())
+        return(info);
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
