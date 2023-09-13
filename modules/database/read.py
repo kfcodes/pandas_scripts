e29ec99db121085d963c953daabd3330778cd6f5
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 16:11:18 +0100

    Updated the function for reading the database using the select statement

--- modules/database/read.py
+++ modules/database/read.py
@@ -8,18 +8,15 @@ def read_to_dataframe(name):
     try:
 
         data = pd.read_sql(os.getenv(name),database_connection())
-        return(data)
+        print(data)
+        # return(data)
 
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
-def read_select_to_dataframe(SELECTION, id):
+def read_selected_data_to_dataframe(selected_data):
     try:
-
-        select_data = os.getenv(SELECTION)+id
-        data = pd.read_sql(select_data ,database_connection())
-
+        data = pd.read_sql(selected_data ,database_connection())
         return(data)
-
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
