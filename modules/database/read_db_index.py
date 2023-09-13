7c5e16c608dfef9ea5749690c787bcc89623c8b6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 11:27:57 +0100

    Improved the format for functions retrieving data from DB

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -2,24 +2,17 @@ from .read import read_select_to_dataframe
 
 def get_production_documentation(id):
     try:
-
-        select = "PRODUCTIONINFO"
-        finished_product_info = read_select_to_dataframe(select, id)
-
+        selection = "PRODUCTIONINFO"
+        finished_product_info = read_select_to_dataframe(selection, id)
         return(finished_product_info);
-
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
 
 def get_product_components(id):
     try:
-
-        select = "COMPONENTS"
-
-        components = read_select_to_dataframe(select, id)
-
+        selection = "COMPONENTS"
+        components = read_select_to_dataframe(selection, id)
         return(components);
-
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
