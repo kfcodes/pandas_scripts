19f1529bb7fa5db4f6bad68dbd3337a26696be7b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-13 16:09:52 +0100

    Using system to pass in values to the function

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,10 +1,16 @@
-# import sys
+import sys
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 import pandas as pd
 from database.read_db_index import get_production_documentation, get_product_components
+# from pdf.create_production_info_pdf import create_production_info_pdf
+# from print.print_pdf import print_a4_pdf
 
-id = "test"
+id = str(sys.argv[1])
 selection = f"{os.getenv('PRODUCTIONINFO')}{id}"
-get_production_documentation(selection)
+info = get_production_documentation(selection)
+print(info)
+# components = get_product_components(product_id);
+# pdf_file = create_production_info_pdf(info, components);
+# print_a4_pdf(pdf_file)
