0dee3222b6bdaafff5714c7036dd6d6c495bc7d8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-14 11:38:58 +0100

    Moved the po and schedule processing modules to functions

--- modules/schedule_file.py
+++ modules/schedule_file.py
@@ -2,6 +2,11 @@ from xlsx.read_xlsx_file import read_xlsx_file_sheet_skip_first_row
 from process_data.process_schedule_data import process_schedule_data
 from database.write_db_index import update_schedule
 
-data= read_xlsx_file_sheet_skip_first_row('SCHEDULEFILE', sheet)
-processed_data = process_schedule_data(data);
-update_schedule(processed_data);
+def process_schedule_file(sheet):
+    try:
+        data= read_xlsx_file_sheet_skip_first_row('SCHEDULEFILE', sheet)
+        processed_data = process_schedule_data(data);
+        update_schedule(processed_data);
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
