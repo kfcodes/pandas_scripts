feb5d934fde4589a581366cbb0d66b4aa80e4747
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-14 11:45:05 +0100

    Created a main.py file to call all the modules and process data with a single call

---
+++ main.py
@@ -0,0 +1,13 @@
+from modules.db_file import process_db_file
+from modules.components_file import process_components_file
+from modules.label_file import process_label_file
+from modules.po_file import process_po_files
+from modules.schedule_file import process_schedule_file
+
+sheet = "test"
+
+process_db_file();
+process_components_file();
+process_label_file();
+process_po_files();
+process_schedule_file(sheet);
