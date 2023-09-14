9a4cc846b663dec5107b6b275b00d054c1862ed8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-14 11:32:16 +0100

    Moved the code to a function that can be called externally

--- modules/db_file.py
+++ modules/db_file.py
@@ -2,6 +2,11 @@ from xlsx.read_xlsx_file import read_full_xlsx_file
 from process_data.process_database_data import process_db_data
 from database.write_db_index import update_db
 
-data= read_full_xlsx_file('DBFILE')
-processed_data = process_db_data(data);
-update_db(processed_data);
+def process_db_file():
+    try:
+        data= read_full_xlsx_file('DBFILE')
+        processed_data = process_db_data(data);
+        update_db(processed_data);
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
