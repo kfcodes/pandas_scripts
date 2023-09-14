d9253397116aab4781be8bcb58bbf8c6f7644487
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-14 11:35:22 +0100

    Moved the components and label modules to functions that can be called externally

--- modules/components_file.py
+++ modules/components_file.py
@@ -2,6 +2,10 @@ from xlsx.read_xlsx_file import read_full_xlsx_file
 from process_data.process_component_data import process_component_data
 from database.write_db_index import update_components
 
-data = read_full_xlsx_file('COMPONENTSFILE')
-processed_data = process_component_data(data);
-update_components(processed_data);
+def process_components_file():
+    try:
+        data = read_full_xlsx_file('COMPONENTSFILE')
+        processed_data = process_component_data(data);
+        update_components(processed_data);
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
--- modules/label_file.py
+++ modules/label_file.py
@@ -5,6 +5,11 @@ import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
-data = read_xlsx_file_by_sheet('LABELSFILE', os.getenv('SHEETNAME'))
-processed_data = process_label_data(data);
-update_labels(processed_data);
+def process_label_file():
+    try:
+        data = read_xlsx_file_by_sheet('LABELSFILE', os.getenv('SHEETNAME'))
+        processed_data = process_label_data(data);
+        update_labels(processed_data);
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
