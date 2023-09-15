e25ab546246bfcec98ef31898570cec3ea7a3b6c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 12:35:08 +0100

    Updated the production info module to process the dictioinary that is returned

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,18 +1,19 @@
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
+import pandas as pd
 from database.read_db_index import get_production_info, get_database_info2
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
 
 def get_component_data(id):
     try:
         product = f"{os.getenv('PRODUCTIONINFO')}{id}"
-        uid, q, w, e, r, p = get_production_info(product)
-        print(f"id: {uid}")
-        print(f"description: {q}")
-        print(f"lot: {w}")
-        print(f"bbe: {e}")
-        print(f"order_id: {r}")
-        print(f"orderQty: {p}")
+        product_info = get_production_info(product)
+        uid = product_info[os.getenv("UID")][0]
+        production_components = f"{os.getenv('COMPONENTS')}'{uid}'"
+        components = get_database_info2(production_components);
+        print(f" Components: {components}")
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
