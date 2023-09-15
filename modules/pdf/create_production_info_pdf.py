ed47328e907300d04a0693b5626e01e09c9cc1c6
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 13:53:33 +0100

    Added more values to be printed into the PDF file

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -5,21 +5,27 @@ load_dotenv("../.env")
 
 def create_production_info_pdf(finished_product, product_components):
     try:
-
-        production_file=os.getenv('PRODUCTIONINFO')
+        production_file=os.getenv('PRODUCTIONINFOFILE')
 
         pdf = FPDF()
         pdf.add_page()
 
         pdf.set_font('Arial', 'B', 16)
-        pdf.cell(40, 10, finished_product)
+        # pdf.cell(40, 10, finished_product)
+        pdf.cell(40, 10, "test")
 
         pdf.set_font('Arial', 'B', 16)
-        pdf.cell(40, 10, product_components)
+        # pdf.cell(40, 10, product_components)
+        pdf.cell(40, 10, "test")
+        pdf.cell(40, 10, "test")
+        pdf.cell(40, 10, "test")
+        pdf.cell(40, 10, "test")
 
         pdf.output(production_file, 'F')
 
-        return(production_file)
+        print("done")
+
+        # return(production_file)
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
