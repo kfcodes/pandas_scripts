f6f1c3bfa8be3fe816a28544f91ca054af1522c0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 16:49:03 +0100

    Added format for data Identifying data to the PDF file

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -5,18 +5,31 @@ load_dotenv("../.env")
 
 def create_production_info_pdf(finished_product, product_components):
     try:
-        production_file=os.getenv('PRODUCTIONINFOFILE')
+
+        lot = f"LOT:    {finished_product[os.getenv('L')][0]}"
+        bbe = f"BBE:    {finished_product[os.getenv('B')][0]}"
+        p = f"ORDER ID:     {str(int(finished_product[os.getenv('P')][0]))}        |          ORDER QTY:    {str(int(finished_product[os.getenv('PQ')][0]))}"
+
+        production_file = os.getenv('PRODUCTIONINFOFILE')
+
         pdf = FPDF()
         pdf.add_page()
 
-        # Print the UID on the PDF
+        # ADD THE PRODUCT INFO TO THE PDF
         pdf.set_font('Arial', 'B', 40)
-        # Centered text in a framed 20*10 mm cell and line break
         pdf.multi_cell(190, 20, finished_product[os.getenv("UID")][0], 1, 'C')
-
-        # Print the Production information
         pdf.set_font('Arial', '', 20)
         pdf.multi_cell(190, 15, finished_product[os.getenv("DESC")][0], 1, 'C')
+
+
+        # ADD THE IDENTIFIER DATA TO THE PDF
+        pdf.set_font('Arial', 'B', 15)
+        pdf.multi_cell(190, 15, lot, 1, 'C')
+        pdf.multi_cell(190, 15, bbe, 1, 'C')
+        pdf.multi_cell(190, 15, p, 1, 'C')
+
+
+        print(product_components)
 
         pdf.output(production_file, 'F')
 
