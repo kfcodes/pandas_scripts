5b5d683dd41b8c4bd453ac054c5976cee392faa1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 12:19:34 +0100

    The read DB function now returns a dictionary with the values from the DB

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -7,16 +7,8 @@ from .connect import database_connection
 def get_production_info(selection):
     try:
         info = pd.read_sql(selection ,database_connection())
-        values = info.values.tolist()[0]
-        id = values[2]
-        description = values[3]
-        lot = values[4]
-        bbe = values[5]
-        order_id = values[1]
-        order_quantity = values[7]
-        return_values = (id, description, lot, bbe, order_id, order_quantity);
-        print(type(return_values));
-        return return_values
+        values = info.to_dict(orient='list')
+        return values
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
