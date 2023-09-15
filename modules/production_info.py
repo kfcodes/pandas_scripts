ba0cc3a987e11332c4057daa77e648711ab3b15b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 11:54:24 +0100

    Updated the production info module to print the values returned

--- modules/production_info.py
+++ modules/production_info.py
@@ -1,16 +1,20 @@
-import sys
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
-import pandas as pd
-from database.read_db_index import get_production_documentation, get_product_components
-# from pdf.create_production_info_pdf import create_production_info_pdf
-# from print.print_pdf import print_a4_pdf
+from database.read_db_index import get_production_info, get_database_info2
 
-id = str(sys.argv[1])
-selection = f"{os.getenv('PRODUCTIONINFO')}{id}"
-info = get_production_documentation(selection)
-print(info)
-# components = get_product_components(product_id);
-# pdf_file = create_production_info_pdf(info, components);
-# print_a4_pdf(pdf_file)
+def get_component_data(id):
+    try:
+        product = f"{os.getenv('PRODUCTIONINFO')}{id}"
+        uid, q, w, e, r, p = get_production_info(product)
+        print(f"id: {uid}")
+        print(f"description: {q}")
+        print(f"lot: {w}")
+        print(f"bbe: {e}")
+        print(f"order_id: {r}")
+        print(f"orderQty: {p}")
+
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+get_component_data("18")
