ecef8979ea37c3ee17acb7ef4c9d592ef745d58a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 10:37:20 +0100

    Updated the read file to return a tuple to the calling function

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -4,11 +4,19 @@ load_dotenv("../../.env")
 import pandas as pd
 from .connect import database_connection
 
-def get_database_info(selection):
+def get_production_info(selection):
     try:
         info = pd.read_sql(selection ,database_connection())
-        id = info['product_id'].to_string(index=False)
-        return(id, info);
+        values = info.values.tolist()[0]
+        id = values[2]
+        description = values[3]
+        lot = values[4]
+        bbe = values[5]
+        order_id = values[1]
+        order_quantity = values[7]
+        return_values = (id, description, lot, bbe, order_id, order_quantity);
+        print(type(return_values));
+        return return_values
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
