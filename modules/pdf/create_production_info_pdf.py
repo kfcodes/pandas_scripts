4c63f55f71f43a0620bbfcb0ed2d6616ef1f3815
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-15 15:25:49 +0100

    Added functionality to add the product data to the page formatted nicely

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -6,20 +6,17 @@ load_dotenv("../.env")
 def create_production_info_pdf(finished_product, product_components):
     try:
         production_file=os.getenv('PRODUCTIONINFOFILE')
-
         pdf = FPDF()
         pdf.add_page()
 
-        pdf.set_font('Arial', 'B', 16)
-        # pdf.cell(40, 10, finished_product)
-        pdf.cell(40, 10, "test")
+        # Print the UID on the PDF
+        pdf.set_font('Arial', 'B', 40)
+        # Centered text in a framed 20*10 mm cell and line break
+        pdf.multi_cell(190, 20, finished_product[os.getenv("UID")][0], 1, 'C')
 
-        pdf.set_font('Arial', 'B', 16)
-        # pdf.cell(40, 10, product_components)
-        pdf.cell(40, 10, "test")
-        pdf.cell(40, 10, "test")
-        pdf.cell(40, 10, "test")
-        pdf.cell(40, 10, "test")
+        # Print the Production information
+        pdf.set_font('Arial', '', 20)
+        pdf.multi_cell(190, 15, finished_product[os.getenv("DESC")][0], 1, 'C')
 
         pdf.output(production_file, 'F')
 
