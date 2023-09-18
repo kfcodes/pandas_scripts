845610df0b696544affc10351290081699a9a2ae
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-18 15:16:47 +0100

    Added the Unique identfiying product information to the PDF

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -5,31 +5,26 @@ load_dotenv("../.env")
 
 def create_production_info_pdf(finished_product, product_components):
     try:
-
-        lot = f"LOT:    {finished_product[os.getenv('L')][0]}"
-        bbe = f"BBE:    {finished_product[os.getenv('B')][0]}"
-        p = f"ORDER ID:     {str(int(finished_product[os.getenv('P')][0]))}        |          ORDER QTY:    {str(int(finished_product[os.getenv('PQ')][0]))}"
-
         production_file = os.getenv('PRODUCTIONINFOFILE')
 
         pdf = FPDF()
         pdf.add_page()
 
-        # ADD THE PRODUCT INFO TO THE PDF
+        # ADD THE UNIQUE PRODUCT IDENTIFIERS TO THE PDF
         pdf.set_font('Arial', 'B', 40)
         pdf.multi_cell(190, 20, finished_product[os.getenv("UID")][0], 1, 'C')
         pdf.set_font('Arial', '', 20)
         pdf.multi_cell(190, 15, finished_product[os.getenv("DESC")][0], 1, 'C')
-
-
-        # ADD THE IDENTIFIER DATA TO THE PDF
+        lot = f"LOT:    {finished_product[os.getenv('L')][0]}"
+        bbe = f"BBE:    {finished_product[os.getenv('B')][0]}"
+        order_info = f"ORDER ID:     {str(int(finished_product[os.getenv('P')][0]))}        |          ORDER QTY:    {str(int(finished_product[os.getenv('PQ')][0]))}"
         pdf.set_font('Arial', 'B', 15)
         pdf.multi_cell(190, 15, lot, 1, 'C')
         pdf.multi_cell(190, 15, bbe, 1, 'C')
-        pdf.multi_cell(190, 15, p, 1, 'C')
+        pdf.multi_cell(190, 15, order_info, 1, 'C')
 
 
-        print(product_components)
+        # print(product_components)
 
         pdf.output(production_file, 'F')
 
