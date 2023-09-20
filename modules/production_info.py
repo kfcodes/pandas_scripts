5895557784ea20400fa6aa90f6024b452d5c8644
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-20 16:44:38 +0100

    Added pretty print to improve the output to the console

--- modules/production_info.py
+++ modules/production_info.py
@@ -2,20 +2,18 @@ import pprint
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
-import pandas as pd
 from database.read_db_index import get_production_info, get_production_components
-from pdf.create_production_info_pdf import create_production_info_pdf
 
 def get_component_data(id):
     try:
+        pp = pprint.PrettyPrinter(indent=4)
         product = f"{os.getenv('PRODUCTIONINFO')}{id}"
         product_info = get_production_info(product)
         uid = product_info[os.getenv("UID")][0]
         production_components = f"{os.getenv('COMPONENTS')}'{uid}'"
-        components = get_production_components(production_components);
-        create_production_info_pdf(product_info, components);
+        pp.pprint(production_components)
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-get_component_data("18")
+get_component_data("3")
