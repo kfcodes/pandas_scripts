73a8b545c0146bbf5d43c86129770bbcdf28cc49
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-20 17:31:45 +0100

    Removed unused print to console code and returns the name of the file

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -25,10 +25,8 @@ def create_production_info_pdf(finished_product, product_components):
         pdf.multi_cell(190, 15, bbe, 1, 'C')
         pdf.multi_cell(190, 15, order_info, 1, 'C')
 
-        pp = pprint.PrettyPrinter(indent=4)
         # ADD PRODUCT COMPONENTS TO THE PDF
-        pp.pprint(product_components)
-
+        pdf.set_font('Arial', '', 11)
         for component in product_components:
             print_info = f" {product_components[component][os.getenv('COMP1')]}  |   {product_components[component][os.getenv('COMP2')]}  |  QTY: {product_components[component][os.getenv('COMP3')]}"
             pdf.multi_cell(190, 20, print_info , 1, 'C')
@@ -37,7 +35,7 @@ def create_production_info_pdf(finished_product, product_components):
 
         print("done")
 
-        # return(production_file)
+        return(production_file)
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
