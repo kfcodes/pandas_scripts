ffc9d8bdd4704507fbce80f590d3c3086587b051
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-21 10:59:43 +0100

    Added functionality to print the PDF file automatically after it is made

--- modules/production_info.py
+++ modules/production_info.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv("../.env")
 from database.read_db_index import get_production_info, get_production_components
 from pdf.create_production_info_pdf import create_production_info_pdf
+from print.print_pdf import print_a4_pdf
 
 def get_component_data(id):
     try:
@@ -14,7 +15,7 @@ def get_component_data(id):
         production_components = f"{os.getenv('COMPONENTS')}'{uid}'"
         components = get_production_components(production_components);
         pdf_file = create_production_info_pdf(product_info, components);
-        pp.pprint(pdf_file)
+        print_a4_pdf(pdf_file)
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
