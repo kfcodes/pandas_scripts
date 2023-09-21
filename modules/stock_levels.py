3d4717c10f2f1df319d8346387d097eb512b3f15
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-21 11:17:32 +0100

    Imported the functions To process the data and the input it into the DB

--- modules/stock_levels.py
+++ modules/stock_levels.py
@@ -1,10 +1,12 @@
 from xlsx.read_xlsx_file import read_full_xlsx_file
+from process_data.process_stock_data import process_stock_data
 from database.write_db_index import update_stock
 
 def process_stock_file():
     try:
         data = read_full_xlsx_file('STOCKFILE')
-        update_stock(data);
+        processed_data = process_stock_data(data);
+        update_stock(processed_data);
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
