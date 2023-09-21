2cd1f6e6d6a735ed8b02dbc6b8bb9acbd2b785c0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-21 16:05:02 +0100

    Improved the format of the data being printed to the PDF file

--- modules/pdf/create_production_info_pdf.py
+++ modules/pdf/create_production_info_pdf.py
@@ -12,24 +12,24 @@ def create_production_info_pdf(finished_product, product_components):
         pdf.add_page()
 
         # ADD THE UNIQUE PRODUCT IDENTIFIERS TO THE PDF
-        pdf.set_font('Arial', 'B', 40)
-        pdf.multi_cell(190, 20, finished_product[os.getenv("UID")][0], 1, 'C')
-
+        pdf.set_font('Arial', 'B', 30)
+        pdf.multi_cell(190, 15, finished_product[os.getenv("UID")][0], 1, 'C')
         pdf.set_font('Arial', '', 20)
         pdf.multi_cell(190, 15, finished_product[os.getenv("DESC")][0], 1, 'C')
         lot = f"LOT:    {finished_product[os.getenv('L')][0]}"
         bbe = f"BBE:    {finished_product[os.getenv('B')][0]}"
-        order_info = f"ORDER ID:     {str(int(finished_product[os.getenv('P')][0]))}        |          ORDER QTY:    {str(int(finished_product[os.getenv('PQ')][0]))}"
+        order_info = f"ORDER ID:     {str(finished_product[os.getenv('P')][0])}        |          ORDER QTY:    {str(finished_product[os.getenv('POQ')][0])}"
         pdf.set_font('Arial', 'B', 15)
-        pdf.multi_cell(190, 15, lot, 1, 'C')
-        pdf.multi_cell(190, 15, bbe, 1, 'C')
-        pdf.multi_cell(190, 15, order_info, 1, 'C')
+        pdf.multi_cell(190, 12, lot, 1, 'C')
+        pdf.multi_cell(190, 12, bbe, 1, 'C')
+        pdf.multi_cell(190, 12, order_info, 1, 'C')
 
         # ADD PRODUCT COMPONENTS TO THE PDF
-        pdf.set_font('Arial', '', 11)
-        for component in product_components:
-            print_info = f" {product_components[component][os.getenv('COMP1')]}  |   {product_components[component][os.getenv('COMP2')]}  |  QTY: {product_components[component][os.getenv('COMP3')]}"
-            pdf.multi_cell(190, 20, print_info , 1, 'C')
+        if product_components != None:
+            pdf.set_font('Arial', '', 11)
+            for component in product_components:
+                print_info = f"CODE:    {product_components[component][os.getenv('COMP1')]}     |  QTY: {product_components[component][os.getenv('COMP3')]} | {product_components[component][os.getenv('COMP2')]}"
+                pdf.multi_cell(190, 15, print_info , 0, 'L')
 
         pdf.output(production_file, 'F')
 
