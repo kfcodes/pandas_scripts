c5d2e2b4efe176cc4570f6a89ceae7493d257168
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-21 11:15:18 +0100

    Added functionality to process the stock data and return the processed data for insertion to the DB

--- modules/process_data/process_stock_data.py
+++ modules/process_data/process_stock_data.py
@@ -1,9 +1,17 @@
+import pandas as pd
+import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
 def process_stock_data(data):
     try:
-        print(data)
+
+        database_data = data.loc[:, [ os.getenv('STOCKINPUT1'), os.getenv('STOCKINPUT2'), os.getenv('STOCKINPUT3'), os.getenv('STOCKINPUT4')]]
+
+        processed_data = database_data.rename(columns={os.getenv('STOCKINPUT1'):os.getenv('STOCKOUTPUT1'),os.getenv('STOCKINPUT2'):os.getenv('STOCKOUTPUT2'),os.getenv('STOCKINPUT3'):os.getenv('STOCKOUTPUT3'),os.getenv('STOCKINPUT4'):os.getenv('STOCKOUTPUT4'),})
+
+        return(processed_data)
+
     except Exception as ex:
 
         print("Data could not be processed: \n", ex)
