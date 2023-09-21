051f0adda354ab48bd20fadc1d1053eed4cf581c
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-21 11:10:43 +0100

    Imported the function to write the stock levels data to the DB

--- modules/stock_levels.py
+++ modules/stock_levels.py
@@ -1,9 +1,10 @@
 from xlsx.read_xlsx_file import read_full_xlsx_file
+from database.write_db_index import update_stock
 
 def process_stock_file():
     try:
         data = read_full_xlsx_file('STOCKFILE')
-        print(data)
+        update_stock(data);
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
