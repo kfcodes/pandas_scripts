5881d2fc81516747e1fde2afcbb723903cc053a2
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-09-25 11:36:21 +0100

    Improved function to print pdf files

--- modules/print/print_pdf.py
+++ modules/print/print_pdf.py
@@ -4,15 +4,13 @@ from dotenv import load_dotenv
 load_dotenv("../../.env")
 
 def print_a4_pdf(file_path):
-
     printer_name = os.getenv('A4PRINTER')
-
-    conn = cups. Connection()
-    printers = conn.getPrinters()
+    printer_connection = cups.Connection()
+    printers = printer_connection.getPrinters()
     
     if printer_name in printers:
         try:
-            conn.printFile(printer_name, file_path, "Print Job", {})
+            printer_connection.printFile(printer_name, file_path, "Print Job", {})
             print("File sent to printer successfully.")
         except cups.IPPError as e:
             print(f"Error printing file: {e}")
