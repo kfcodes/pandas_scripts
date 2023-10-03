ef19216e15d8ef4b45073173ace2b041248d0d85
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-10-03 10:51:59 +0100

    Moved sheet name to .env variable

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 from modules.db_file import process_db_file
 from modules.components_file import process_components_file
@@ -5,9 +6,8 @@ from modules.label_file import process_label_file
 from modules.po_file import process_po_files
 from modules.schedule_file import process_schedule_file
 
-
 load_dotenv(".env")
-# sheet = "test"
+sheet = os.getenv("sheet");
 
 process_db_file()
 process_components_file();
