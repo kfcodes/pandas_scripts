e20d7e234c274cf5055a18fb5e86560c11ee69b7
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-10-09 15:06:04 +0100

    Added function to read the label info from the DB

--- modules/database/read_db_index.py
+++ modules/database/read_db_index.py
@@ -18,3 +18,11 @@ def get_production_components(selection):
         return values
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
+
+def get_label_data(selection):
+    try:
+        info = pd.read_sql(selection ,database_connection())
+        values = info.to_dict(orient='list')
+        return values
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
