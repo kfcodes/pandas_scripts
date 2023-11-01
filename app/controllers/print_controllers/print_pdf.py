af67df6fc6f51af95ac0bad5d5fc593c9923d1fa
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-01 16:29:48 +0000

    Updated imports and file structures for zpl and print controllers

--- app/controllers/print_controllers/print_pdf.py
+++ app/controllers/print_controllers/print_pdf.py
@@ -1,18 +1,19 @@
 import cups
+
 import os
 from dotenv import load_dotenv
-load_dotenv(".env")
+load_dotenv("../../../.env")
 
-def print_a4_pdf(file_path):
-    printer_name = os.getenv('A4PRINTER')
+def print_a4_pdf(file):
+    printer = os.getenv('A4PRINTER')
     printer_connection = cups.Connection()
     printers = printer_connection.getPrinters()
     
-    if printer_name in printers:
+    if printer in printers:
         try:
-            printer_connection.printFile(printer_name, file_path, "Print Job", {})
-            print("File sent to printer successfully.")
+            printer_connection.printFile(printer, file, "Print Job", {})
+            print("File sent to printer")
         except cups.IPPError as e:
             print(f"Error printing file: {e}")
     else:
-        print(f"Printer '{printer_name}' not found.")
+        print(f"Printer '{printer}' not found.")
--- app/controllers/zpl_controllers/create_label.py
+++ app/controllers/zpl_controllers/create_label.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-load_dotenv(".env")
+load_dotenv("../../../.env")
 
-def create_production_label_file(label_info):
+def create_label(label_info):
     try:
         print(label_info)
         return("LABEL CREATED")
