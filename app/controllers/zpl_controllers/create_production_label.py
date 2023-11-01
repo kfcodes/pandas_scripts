a119eef6b29be19205af6b019be8e5da6644b5db
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-01 14:39:52 +0000

    Moved pandas data analysis controllers to a dedicated directory

--- app/controllers/zpl_controllers/create_production_label.py
+++ app/controllers/zpl_controllers/create_production_label.py
@@ -4,6 +4,7 @@ load_dotenv(".env")
 
 def create_production_label_file(label_info):
     try:
+        print(label_info)
         return("LABEL CREATED")
     except Exception as ex:
         print("Data could not be processed: \n", ex)
