8920c5ede8e986beb4756f36341de2b830a770f7
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-04 22:19:53 +0000

    Removed unused modules and rename pandas controller directory

--- app/modules/xml/process_xml.py
+++
@@ -1,8 +0,0 @@
-import xmltodict
-
-with open('../test.xml') as fd:
-    xml_dict = xmltodict.parse(fd.read(), process_namespaces=True)
-    for product in xml_dict['BOMRecords']['BOMRecord']:
-        product_code = product['Reference']
-        product_description = product['Description']
-        print( product_code + ' - ' + product_description )
