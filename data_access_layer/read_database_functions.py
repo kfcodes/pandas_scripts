3373d30d4539e4a50e04d4e9bab54cad917d723a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-05 15:27:20 +0000

    combined functions to read and write files

--- data_access_layer/read_database_functions.py
+++ data_access_layer/read_database_functions.py
@@ -1,9 +1,9 @@
 import pandas as pd
-from .connect import database_connection
+from .database_connection import database_connection
 
 import os
 from dotenv import load_dotenv
-load_dotenv("../../../.env")
+load_dotenv("../.env")
 
 def read_to_dataframe(name):
     try:
@@ -16,5 +16,29 @@ def read_selected_data_to_dataframe(selected_data):
     try:
         data = pd.read_sql(selected_data ,database_connection())
         return(data)
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
+def get_production_info(selection):
+    try:
+        info = pd.read_sql(selection ,database_connection())
+        values = info.to_dict(orient='list')
+        return values
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
+def get_production_components(selection):
+    try:
+        info = pd.read_sql(selection ,database_connection())
+        values = info.to_dict(orient='index')
+        return values
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
+def get_label_data(selection):
+    try:
+        info = pd.read_sql(selection ,database_connection())
+        values = info.to_dict(orient='list')
+        return values
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
