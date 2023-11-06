54a0281327f62243980754f7695255d2cf7d7a5b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-06 16:18:06 +0000

    Updated the main.py file to only do label data

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from app.controllers.data_processing_controllers.db_file import process_db_file
-from app.controllers.data_processing_controllers.components_file import process_components_file
-from app.controllers.data_processing_controllers.label_file import process_label_file
-from app.controllers.data_processing_controllers.po_file import process_po_files
-from app.controllers.data_processing_controllers.schedule_file import process_schedule_file
+# from app.controllers.data_processing_controllers.db_file import process_db_file
+# from app.controllers.data_processing_controllers.components_file import process_components_file
+from presentation_layer.data_processing_controllers.label_data import process_label_file
+# from app.controllers.data_processing_controllers.label_file import process_label_file
+# from app.controllers.data_processing_controllers.po_file import process_po_files
+# from app.controllers.data_processing_controllers.schedule_file import process_schedule_file
 
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
+
 sheet = os.getenv("SHEETINPUT");
 
-process_db_file()
-process_components_file();
+# process_db_file()
+# process_components_file();
 process_label_file();
-process_po_files();
-process_schedule_file(sheet);
+# process_po_files();
+# process_schedule_file(sheet);
