c5c472f3722481077407993e5c5687afc357b1ac
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-07 12:00:20 +0000

    Commented out code for testing purposes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# from app.controllers.data_processing_controllers.db_file import process_db_file
-# from app.controllers.data_processing_controllers.components_file import process_components_file
+from presentation_layer.data_processing_controllers.database_data import process_db_file
 from presentation_layer.data_processing_controllers.label_data import process_label_file
-# from app.controllers.data_processing_controllers.label_file import process_label_file
-# from app.controllers.data_processing_controllers.po_file import process_po_files
-# from app.controllers.data_processing_controllers.schedule_file import process_schedule_file
+# from presentation_layer.data_processing_controllers.product_components import process_components_file
+# from presentation_layer.data_processing_controllers.po_data import process_po_files
+# from presentation_layer.data_processing_controllers.schedule_data import process_schedule_file
 
 import os
 from dotenv import load_dotenv
@@ -11,7 +10,7 @@ load_dotenv(".env")
 
 sheet = os.getenv("SHEETINPUT");
 
-# process_db_file()
+process_db_file()
 # process_components_file();
 process_label_file();
 # process_po_files();
