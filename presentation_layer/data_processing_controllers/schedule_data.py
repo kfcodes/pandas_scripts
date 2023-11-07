c0f3049fc7e0a58cbc5caf75ff95ef05b3584359
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-07 11:59:07 +0000

    Moved module import paths to absolute paths

--- presentation_layer/data_processing_controllers/schedule_data.py
+++ presentation_layer/data_processing_controllers/schedule_data.py
@@ -1,15 +1,10 @@
-from ..xlsx_controllers.read_xlsx_file import read_data_after_first_row
-from ..pandas_controllers.process_schedule_data import process_schedule_data
-from ..database_controllers.write_db_index import update_schedule
-
-import os
-from dotenv import load_dotenv
-load_dotenv("../../../.env")
+from business_logic_layer.xlsx_controllers.read_xlsx_file import read_data_after_first_row
+from business_logic_layer.pandas_controllers.process_schedule_data import process_schedule_data
+from data_access_layer.write_database_functions import update_schedule
 
 def process_schedule_file(sheet):
     
     try:
-
         data= read_data_after_first_row('SCHEDULEFILE', sheet)
         processed_data = process_schedule_data(data);
         update_schedule(processed_data);
