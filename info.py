6416fc9957644980af96cea3ed37f76adf3969ef
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-09 14:41:29 +0000

    Updated the info module to use the new file structure

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
 import sys
-from app.controllers.production_info import get_component_data
+from .business_logic_layer.production_documentation_controllers import get_component_data
 
 id = str(sys.argv[1])
 get_component_data(id);
