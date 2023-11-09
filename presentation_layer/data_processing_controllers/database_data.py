8ff5f118e3908508716e7d1c2ebdad63079bb4fb
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-09 14:01:14 +0000

    Updated the database data processing module

--- presentation_layer/data_processing_controllers/database_data.py
+++ presentation_layer/data_processing_controllers/database_data.py
@@ -5,9 +5,9 @@ from data_access_layer.write_database_functions import update_db
 def process_db_file():
 
     try:
+
         data= read_data('DBFILE')
         processed_data = process_db_data(data);
-        # print(processed_data)
         update_db(processed_data);
 
     except Exception as ex:
