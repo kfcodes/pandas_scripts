4d6601d44038268d64ecc8029acd036fda934ec9
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-09 14:43:57 +0000

    Added functionality to read the database information into a pandas dataframe

--- presentation_layer/packing_list_controllers/get_packing_list_data_controllers.py
+++ presentation_layer/packing_list_controllers/get_packing_list_data_controllers.py
@@ -1,4 +1,4 @@
-from ...data_access_layer.read_database_functions import read_selected_data_to_dataframe
+from data_access_layer.read_database_functions import read_to_dataframe
 
 
 def get_all_packing_lists():
@@ -6,12 +6,14 @@ def get_all_packing_lists():
     try:
 
         # Read the data from the database
-        packing_lists = read_data('DBFILE')
+        packing_lists = read_to_dataframe('PACKINGLISTS')
+        # get_packing_lists_data()
+        print(packing_lists)
 
         # Processs the data into html
-        html_data = data_html(packing_lists);
+        # html_data = data_html(packing_lists);
 
-        return(html_data);
+        # return(html_data);
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
