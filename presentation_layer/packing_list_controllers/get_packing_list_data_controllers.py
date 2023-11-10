b0bc33cb85aaaa766b36b3b749d368725b3f62f1
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-10 15:48:27 +0000

    Added the controllers to the presentation directory for controlling the data for the html files for the packing list information

--- presentation_layer/packing_list_controllers/get_packing_list_data_controllers.py
+++ presentation_layer/packing_list_controllers/get_packing_list_data_controllers.py
@@ -1,21 +1,31 @@
-from data_access_layer.read_database_functions import read_to_dataframe
+from fastapi.responses import HTMLResponse
+# from data_access_layer.read_database_functions import read_to_dataframe
+# from business_logic_layer.html_controllers.create_scanner_html import create_packing_list_html
+from ...data_access_layer.read_database_functions import read_to_dataframe
+from ...business_logic_layer.html_controllers.create_scanner_html import packing_lists_html, pallet_list_html, pallet_info_html
 
 def get_all_packing_lists():
-
     try:
-
-        # Read the data from the database
         packing_lists = read_to_dataframe('PACKINGLISTS')
-        print(packing_lists)
+        html_data = packing_lists_html(packing_lists);
+        return html_data
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
 
-        # Processs the data into html
-        # html_data = data_html(packing_lists);
+def get_packing_list(id):
+    try:
+        pallets = read_to_dataframe('PALLETS')
+        html_data = pallet_list_html(pallets);
+        return html_data
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
 
-        # return(html_data);
+def get_pallet_info(id):
+    try:
+        pallet_info = read_to_dataframe('PACKINGLISTS')
+        html_data = pallet_info_html(pallet_info);
+        return html_data
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-import os
-from dotenv import load_dotenv
-load_dotenv(".env")
