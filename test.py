1a0a1cf316e1716218b5df5c09a775d075c195f8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-10 11:44:57 +0000

    Moved functions to test.py module

--- test.py
+++ test.py
@@ -3,13 +3,15 @@ from presentation_layer.data_processing_controllers.label_data import process_la
 from presentation_layer.data_processing_controllers.product_components import process_components_file
 from presentation_layer.data_processing_controllers.po_data import process_po_files
 from presentation_layer.data_processing_controllers.schedule_data import process_schedule_file
+from presentation_layer.packing_list_controllers.get_packing_list_data_controllers import get_all_packing_lists
 
 import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-process_db_file()
-process_label_file();
-process_components_file();
+# process_db_file()
+# process_label_file();
+# process_components_file();
 # process_po_files();
 # process_schedule_file(sheet);
+get_all_packing_lists()
