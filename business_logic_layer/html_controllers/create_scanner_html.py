f341d31e03881db8a0d9d8697dff46d2aef45983
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-10 15:47:48 +0000

    Added functions for creating the HTML files for the new routes

--- business_logic_layer/html_controllers/create_scanner_html.py
+++ business_logic_layer/html_controllers/create_scanner_html.py
@@ -2,11 +2,24 @@ from business_logic_layer.html_controllers.scanner_packing_lists_header import s
 from business_logic_layer.html_controllers.scanner_packing_lists_list import packing_lists
 from business_logic_layer.html_controllers.close_html import close_html
 
-def create_packing_list_html():
+def packing_lists_html(packing_lists):
     start = scanner_packing_lists_header();
     middle = packing_lists();
     end = close_html();
-    
+    html_file = f"{start}{middle}{end}"
+    return html_file
+
+def pallet_list_html(pallets):
+    start = scanner_packing_lists_header();
+    middle = packing_lists(packing_lists);
+    end = close_html();
+    html_file = f"{start}{middle}{end}"
+    return html_file
+
+def pallet_info_html(pallet_info):
+    start = scanner_packing_lists_header();
+    middle = packing_lists();
+    end = close_html();
     html_file = f"{start}{middle}{end}"
 
     return html_file
