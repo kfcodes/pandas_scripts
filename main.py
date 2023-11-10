a59090b391574d3dc9abb5c1ced6c57e3fa75b9e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-10 15:45:56 +0000

    Added routes to get pallet and pallet list from db

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from presentation_layer.packing_list_controllers.get_packing_list_data_controllers import get_all_packing_lists
+from presentation_layer.packing_list_controllers.get_packing_list_data_controllers import get_packing_list
+from presentation_layer.packing_list_controllers.get_packing_list_data_controllers import get_pallet_info
 
 import os
 from dotenv import load_dotenv
@@ -26,20 +28,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def generate_html_response():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! Packing LISTS!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
 @app.get("/", response_class=HTMLResponse)
-async def read_items():
-    get_all_packing_lists();
-    return generate_html_response()
+async def scanner_packing_lists():
+    html_data = get_all_packing_lists();
+    return HTMLResponse(content=html_data, status_code=200)
+
+@app.get("/packing_list/{id}", response_class=HTMLResponse)
+async def scanner_packing_list(id):
+    html_data = get_packing_list(id);
+    return HTMLResponse(content=html_data, status_code=200)
+
+@app.get("/pallet/{id}", response_class=HTMLResponse)
+async def scanner_pallet_info(id):
+    html_data = get_pallet_info(id);
+    return HTMLResponse(content=html_data, status_code=200)
