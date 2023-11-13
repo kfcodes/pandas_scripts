10ccadada85fcb8b50fa74b870a9f10df00d9ede
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-13 16:49:30 +0000

    Created simple headers for the html files for the scanner apis

---
+++ business_logic_layer/html_controllers/scanner_html/pallet_info_header.py
@@ -0,0 +1,9 @@
+def scanner_info_header():
+    html = """
+            <html>
+                <head>
+                    <title>Pallet Information</title>
+                </head>
+            <body>
+        """
+    return html
---
+++ business_logic_layer/html_controllers/scanner_html/pallet_list_header.py
@@ -0,0 +1,9 @@
+def scanner_pallet_list_header():
+    html = """
+            <html>
+                <head>
+                    <title>Packing list Pallets</title>
+                </head>
+            <body>
+        """
+    return html
