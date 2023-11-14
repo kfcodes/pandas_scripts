ebb64a05f878235cfd089dc27ffc6a5777651584
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-14 14:35:33 +0000

    Removed relative imports for the production controllers

--- presentation_layer/html_controllers/scanner_html_controllers.py
+++ presentation_layer/html_controllers/scanner_html_controllers.py
@@ -6,14 +6,6 @@ from business_logic_layer.html_controllers.scanner_html.pallet_id_list import pa
 from business_logic_layer.html_controllers.scanner_html.pallet_info_header import scanner_info_header
 from business_logic_layer.html_controllers.scanner_html.pallet_info import packing_lists
 
-# from ...business_logic_layer.html_controllers.scanner_html.packing_list_header import scanner_packing_lists_header
-# from ...business_logic_layer.html_controllers.scanner_html.packing_list_options import packing_lists
-# from ...business_logic_layer.html_controllers.scanner_html.close_html import close_html
-# from ...business_logic_layer.html_controllers.scanner_html.pallet_list_header import scanner_pallet_list_header
-# from ...business_logic_layer.html_controllers.scanner_html.pallet_id_list import pallet_list
-# from ...business_logic_layer.html_controllers.scanner_html.pallet_info_header import scanner_info_header
-# from ...business_logic_layer.html_controllers.scanner_html.pallet_info import packing_lists
-
 def packing_lists_html(list):
     start = scanner_packing_lists_header();
     middle = packing_lists(list);
@@ -21,9 +13,9 @@ def packing_lists_html(list):
     html_file = f"{start}{middle}{end}"
     return html_file
 
-def pallet_list_html(pallets):
+def pallet_list_html(palls):
     start = scanner_pallet_list_header();
-    middle = pallet_list(pallets);
+    middle = pallet_list(palls);
     end = close_html();
     html_file = f"{start}{middle}{end}"
     return html_file
--- presentation_layer/scanner_controllers/get_packing_list_data_controllers.py
+++ presentation_layer/scanner_controllers/get_packing_list_data_controllers.py
@@ -1,9 +1,6 @@
 from data_access_layer.read_database_functions import read_to_dataframe, read_selected_data_to_dataframe
 from presentation_layer.html_controllers.scanner_html_controllers import packing_lists_html, pallet_list_html, pallet_info_html
 
-# from ...data_access_layer.read_database_functions import read_to_dataframe, read_selected_data_to_dataframe
-# from  ...presentation_layer.html_controllers.scanner_html_controllers import packing_lists_html, pallet_list_html, pallet_info_html
-
 import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
@@ -29,7 +26,8 @@ def get_pallet_info(id):
     try:
         required_pallet_info = f"{os.getenv('SCANNERPALLETINFO')}{id}"
         pallet_info = read_selected_data_to_dataframe(required_pallet_info)
-        html_data = pallet_info_html(pallet_info);
-        return html_data
+        print(pallet_info)
+        # html_data = pallet_info_html(pallet_info);
+        # return html_data
     except Exception as ex:
         print("Data could not be processed: \n", ex)
