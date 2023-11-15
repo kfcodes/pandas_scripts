94781778242001ec92b3203cccad75674bddf40a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-15 12:17:27 +0000

    Added input for the pallet id value that can be sent to the server with post api

--- business_logic_layer/html_controllers/scanner_html/pallet_id_list.py
+++ business_logic_layer/html_controllers/scanner_html/pallet_id_list.py
@@ -1,13 +1,31 @@
 def pallet_list(pallets):
+
     html = ""
+    html = """
+             <hr>
+             <form method='post' action='/pallet'>
+                  <table>
+                    <tr>
+                      <td><input id='id' name='id' value=''></td>
+                    </tr>
+                  </table>
+                  <input type='submit' name='submit' value='Submit'>
+                </form>
+                <script>
+                  window.onload = function() { document.getElementById('id').focus(); }
+                </script>
+            """
     
     for row in pallets.iterrows():
-
         html = f"""
             {html}
-                        {row[1]['pallet_id']} 
+                    {row[1]['pallet_id']} 
             </br>
         """
 
-        html = f"{html} </ul>"
+    html = html + """
+             <hr>
+            </ul>
+            """
+
     return html
