0a17b530f7ac961ef1e97f6e08fd62a3fe6937d8
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-16 14:00:38 +0000

    Created a module to display the pallet in HTML

---
+++ business_logic_layer/html_controllers/scanner_html/pallet_info.py
@@ -0,0 +1,36 @@
+def pallet_info(info):
+    
+    for row in info.iterrows():
+
+        id = row[1]["id"]
+        weight = row[1]["GrossWeight"]
+        height = row[1]["GrossHeight"]
+        size = row[1]["PalletType"]
+    
+        print(type(info))
+        html = f"""
+
+                    <H1>
+                        {id} 
+                    </H1>
+                    <h3>
+                        {size} 
+                    </h3>
+                    <h4>
+                        {height} (cm) 
+                    </h4>
+                    <h4>
+                        {weight} (kg) 
+                    </h4>
+
+                    </br>
+
+                    <a href="/load_pallet/{id}"
+                        <button>
+                            LOAD PALLET
+                        </button>
+                    </a>
+
+        """
+
+        return html
