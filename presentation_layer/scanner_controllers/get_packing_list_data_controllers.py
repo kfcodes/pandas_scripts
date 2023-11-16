054ae49485a74072f5447d153c479687f486399b
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-16 13:54:15 +0000

    Added path to load the pallet in the DB

--- presentation_layer/scanner_controllers/get_packing_list_data_controllers.py
+++ presentation_layer/scanner_controllers/get_packing_list_data_controllers.py
@@ -1,4 +1,5 @@
-from data_access_layer.read_database_functions import read_to_dataframe, read_selected_data_to_dataframe
+from data_access_layer.read_database_functions import read_to_dataframe, read_selected_data_to_dataframe 
+# from data_access_layer.write_database_functions import update_data
 from presentation_layer.html_controllers.scanner_html_controllers import packing_lists_html, pallet_list_html, pallet_info_html
 
 import os
@@ -29,9 +30,21 @@ def get_pallet_info(id):
         id = body_id_2[0]
         required_pallet_info = f"{os.getenv('SCANNERPALLETINFO')}{id}"
         pallet_info = read_selected_data_to_dataframe(required_pallet_info)
-        print(pallet_info)
-        # html_data = pallet_info_html(pallet_info);
-        html_data = f"<h1>{id}</h1>"
+        html_data = pallet_info_html(pallet_info);
         return html_data
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+def load_pallet_and_get_packing_list(id):
+    try:
+        print("done")
+        # SET THE PALLET TO DISPATCHED 
+        # packing_list_id = f"{os.getenv('SCANNERPALLETS')}{id}"
+        # update_data(id)
+        # GET THE REMAINING PALLETS ON THE PACKING LIST
+        # required_pallets = f"{os.getenv('SCANNERPALLETS')}{id}"
+        # pallets = read_selected_data_to_dataframe(required_pallets)
+        # html_data = pallet_list_html(pallets);
+        # return html_data
     except Exception as ex:
         print("Data could not be processed: \n", ex)
