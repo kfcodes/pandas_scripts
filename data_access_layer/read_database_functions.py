dadd13b7b39dbd9e9103cf22661c58b385a28eb0
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-16 16:04:38 +0000

    Created a function to retrieve data into a list format

--- data_access_layer/read_database_functions.py
+++ data_access_layer/read_database_functions.py
@@ -24,6 +24,14 @@ def get_production_info(selection):
     except Exception as ex:
         print("Connection could not be made due to the following error: \n", ex)
 
+def read_selection_to_list(selection):
+    try:
+        info = pd.read_sql(selection ,database_connection())
+        values = info.to_dict(orient='list')
+        return values
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
+
 def get_production_components(selection):
     try:
         info = pd.read_sql(selection ,database_connection())
