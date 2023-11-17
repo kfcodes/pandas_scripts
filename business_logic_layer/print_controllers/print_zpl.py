679b967eb8f0be6d05917444422237d564410e3a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-17 12:51:22 +0000

    Added function to print small labels to assigned printer over the network

--- business_logic_layer/print_controllers/print_zpl.py
+++ business_logic_layer/print_controllers/print_zpl.py
@@ -15,3 +15,15 @@ def print_large_label():
 
 	except Exception as ex:
 		print("Could not print label due to the following: \n", ex)
+
+def print_small_label():
+	try:
+		host = os.getenv("SMALLLABELPRINTER1") 
+		port = os.getenv("SMALLLABELPRINTERPORT1") 
+		# mysocket.connect((host, port)) #connecting to host
+		# mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ")#using bytes
+		# mysocket.close () #closing connection
+		print(f"Printed the Label to {host}, {port}")
+
+	except Exception as ex:
+		print("Could not print label due to the following: \n", ex)
