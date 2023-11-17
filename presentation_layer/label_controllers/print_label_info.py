477f6b925d4485eb964b15a54aa0aa6a473f211d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-17 15:45:18 +0000

    Importing and printing the small label from the presentation layer controller

--- presentation_layer/label_controllers/print_label_info.py
+++ presentation_layer/label_controllers/print_label_info.py
@@ -1,7 +1,7 @@
 # from ...data_access_layer.read_database_functions import get_label_data
 # from ...business_logic_layer.zpl_controllers.create_small_label import create_small_label_data,  create_small_label_outline
 from business_logic_layer.zpl_controllers.create_small_label import create_small_label_data,  create_small_label_outline
-from business_logic_layer.print_controllers.print_zpl import print_zpl_file
+from business_logic_layer.print_controllers.print_zpl import print_small_label
 
 import os
 from dotenv import load_dotenv
@@ -19,9 +19,7 @@ def print_small_label_with_data():
     
         label_data = outline + body
 
-        print(label_data)
-
-        print_zpl_file(label_file)
+        print_small_label(label_data)
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
