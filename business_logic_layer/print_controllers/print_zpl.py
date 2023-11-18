f320eb857f701e0560a8fd9d9591f3334a171b83
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-18 20:01:01 +0000

    Added functions to print pallet labels and larger product labels to networked printers

--- business_logic_layer/print_controllers/print_zpl.py
+++ business_logic_layer/print_controllers/print_zpl.py
@@ -6,15 +6,16 @@ load_dotenv(".env")
 
 mysocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)         
 
-def print_large_label(label_data):
+def print_large_product_label(label_data):
 	try:
 		host = os.getenv("BIGLABELID1") 
 		port = os.getenv("BIGLABELPORT1") 
-		# mysocket.connect((host, port)) #connecting to host
-		# mysocket.send(b"^XA^A0N,50,50^FO50,50^FDSocket Test^FS^XZ")#using bytes
-		# mysocket.close () #closing connection
-		print(f"Printed the Label to {host}, {port} \n {label_data}")
-
+		port = int(port)
+		label =  label_data.encode(encoding="ascii",errors="ignore")
+		mysocket.connect((host, port)) #connecting to host
+		mysocket.send(label)#using bytes
+		mysocket.close () #closing connection
+		print(f"Printed the Label to {host}, {port}")
 	except Exception as ex:
 		print("Could not print label due to the following: \n", ex)
 
@@ -31,5 +32,18 @@ def print_small_label(label_data):
 		# print(f"Printed the Label to {host1}, {port1} \n {label}")
 		print(f"Printed the Label to {host1}, {port1}")
 
+	except Exception as ex:
+		print("Could not print label due to the following: \n", ex)
+
+def print_pallet_label(label_data):
+	try:
+		host = os.getenv("BIGLABELID1") 
+		port = os.getenv("BIGLABELPORT1") 
+		port = int(port)
+		label =  label_data.encode(encoding="ascii",errors="ignore")
+		mysocket.connect((host, port)) #connecting to host
+		mysocket.send(label)#using bytes
+		mysocket.close () #closing connection
+		print(f"Printed the Label to {host}, {port}")
 	except Exception as ex:
 		print("Could not print label due to the following: \n", ex)
