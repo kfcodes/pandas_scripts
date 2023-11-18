b41969db0b5007c35c16949a91e522455790ef19
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-18 19:47:54 +0000

    Removed unused code from the module for printing labels

--- presentation_layer/label_controllers/print_label_info.py
+++ presentation_layer/label_controllers/print_label_info.py
@@ -1,5 +1,4 @@
 # from ...data_access_layer.read_database_functions import get_label_data
-# from ...business_logic_layer.zpl_controllers.create_small_label import create_small_label_data,  create_small_label_outline
 from business_logic_layer.zpl_controllers.create_small_label import create_small_label_data,  create_small_label_outline
 from business_logic_layer.print_controllers.print_zpl import print_small_label
 
@@ -13,12 +12,9 @@ def print_small_label_with_data():
         
         # label_info = get_label_data(f"{os.getenv('PRODUCTIONLABELINFO')}{id}")
         label_info = "test"
-
         outline = create_small_label_outline()
         body = create_small_label_data(label_info)
-    
         label_data = outline + body
-
         print_small_label(label_data)
 
     except Exception as ex:
