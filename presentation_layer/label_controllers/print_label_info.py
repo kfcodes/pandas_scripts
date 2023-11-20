8d7e06c56e5e9831511a225fd5a3fc05a69570ee
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-20 00:05:48 +0000

    Updated presentation layer functions to print small and large product labels to networked printers

--- presentation_layer/label_controllers/print_label_info.py
+++ presentation_layer/label_controllers/print_label_info.py
@@ -2,20 +2,20 @@ from data_access_layer.read_database_functions import get_label_data
 from business_logic_layer.zpl_controllers.create_small_label import create_small_label_data,  create_small_label_outline
 from business_logic_layer.zpl_controllers.create_large_product_label import create_large_product_label_data,  create_large_product_label_outline
 from business_logic_layer.zpl_controllers.create_large_pallet_label import create_pallet_label_data_part_1, create_pallet_label_data_part_2, create_pallet_label_outline
-from business_logic_layer.print_controllers.print_zpl import print_small_label, print_large_product_label, print_pallet_label
+from business_logic_layer.print_controllers.print_zpl import print_small_label, print_large_label
 
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
-async def print_small_product_label():
+async def print_small_product_label(id):
     try:
-        # label_info = get_label_data(f"{os.getenv('PRODUCTIONLABELINFO')}{id}")
-        label_info = "test"
+        label_info = get_label_data(f"{os.getenv('PRODUCTIONLABELINFO')}{id}")
         outline = create_small_label_outline()
-        body = create_small_label_data(label_info)
+        body = create_small_label_data(label_info, 1)
         label_data = outline + body
-        print_small_label(label_data)
+        print(label_data)
+        # print_small_label(label_data)
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
@@ -25,8 +25,7 @@ async def print_large_product_label(id):
         outline = create_large_product_label_outline()
         body = create_large_product_label_data(label_info, 1)
         label_data = outline + body
-        print(label_data)
-        # print_large_product_label(label_data)
+        print_large_label(label_data)
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
