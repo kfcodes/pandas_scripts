504b438df8d43e971d73a5abf1921ce2130a06ba
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-24 22:50:06 +0000

    Created function to added unique information to pallet label

--- business_logic_layer/zpl_controllers/create_pallet_label.py
+++ business_logic_layer/zpl_controllers/create_pallet_label.py
@@ -18,3 +18,22 @@ def create_pallet_label_outline():
         return(zpl)
     except Exception as ex:
         print("Data could not be processed: \n", ex)
+
+def create_pallet_label_data_part_1(label_info):
+    try:
+        pallet_label_field_1 = os.getenv("PALLETLABEL1")
+        pallet_label_field_2 = os.getenv("PALLETLABEL2")
+        pallet_label_field_3 = os.getenv("PALLETLABEL3")
+        pallet_label_field_4 = os.getenv("PALLETLABEL4")
+        pallet_label_field_5 = os.getenv("PALLETLABEL5")
+        zpl = f"""
+                ^FO660,250^BY5 ^BC90,120,Y,N,N^FD{label_info[pallet_label_field_1][0]}^FS
+                ^FO60,150^A0,80^FD^FS^FD{label_info[pallet_label_field_2][0]}^FS
+                ^FO490,20^A0,60^FD^FS^FD{label_info[pallet_label_field_3][0]}^FS
+                ^FO490,450^A0,60^FD^FS^FD{label_info[pallet_label_field_4][0]}^FS
+                ^FO490,900^A0,60^FD^FS^FD{label_info[pallet_label_field_5][0]}^FS
+                ^XZ
+                """
+        return(zpl)
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
