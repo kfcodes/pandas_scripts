a51ad065902cf43efebe8d22d9470c9e445fd260
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-11-26 23:51:34 +0000

    Created the functions for CRUD operations on the finished products

--- presentation_layer/finished_product_controllers/finished_product_controllers.py
+++ presentation_layer/finished_product_controllers/finished_product_controllers.py
@@ -1,40 +1,41 @@
 from data_access_layer.read_database_functions import read_selection_to_list
+from data_access_layer.write_database_functions import db
 
 import os
 from dotenv import load_dotenv
-load_dotenv("../.env")
+load_dotenv("../../.env")
 
 async def create_finished_product():
     try:
-        finished_product_id = read_selection_to_list(f"{os.getenv('')}")
-        return finished_product_id
+        new_finished_product_id = db(f"{os.getenv('CREATEFINISHEDPRODUCT')}")
+        return new_finished_product_id
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_finished_product_by_id(id):
+async def get_finished_product(product_id):
     try:
-        finished_product = read_selection_to_list(f"{os.getenv('')}'{id}'")
+        finished_product = read_selection_to_list(f"{os.getenv('GETFINISHEDPRODUCT')}'{product_id}'")
         return finished_product
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def update_finished_product_by_id(id):
+async def update_finished_product(product_id):
     try:
-        finished_product = read_selection_to_list(f"{os.getenv('')}'{id}'")
-        return finished_product
+        updated_finished_product = db(f"{os.getenv('UPDATEFINISHEDPRODUCT')}'{product_id}'")
+        return updated_finished_product
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def delete_finished_product_by_id(id):
+async def delete_finished_product_by_id(product_id):
     try:
-        finished_product = read_selection_to_list(f"{os.getenv('')}'{id}'")
-        return finished_product
+        finished_product_info = read_selection_to_list(f"{os.getenv('DELETEFINISHEDPRODUCT')}'{product_id}'")
+        return finished_product_info
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
 async def get_finished_product_group(group_id):
     try:
-        finished_products = read_selection_to_list(f"{os.getenv('')}'{group_id}'")
+        finished_products = read_selection_to_list(f"{os.getenv('FINISHEDPRODUCTGROUP')}'{group_id}'")
         return finished_products
     except Exception as ex:
         print("Data could not be processed: \n", ex)
