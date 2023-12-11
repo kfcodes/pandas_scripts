ce1dd721a53fd3f6288a8d107d40296f7e357424
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-11 14:30:10 +0000

    Moved script tags to head

--- presentation_layer/production_overview_controllers/production_overview_controllers.py
+++ presentation_layer/production_overview_controllers/production_overview_controllers.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
 
-def get_production_overview():
+def update_production_overview():
     try:
 
         html = """
@@ -14,24 +14,10 @@ def get_production_overview():
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Websocket Demo</title>
+        <title>Production Overview</title>
            <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    </head>
-    <body>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <div class="container mt-3">
-        <h1>Current Production</h1>
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" class="form-control" id="messageText" autocomplete="off"/>
-            <button class="btn btn-outline-primary mt-2">Send</button>
-        </form>
-        <hr>
-        <H2 id='product' class="mt-5">
-        You Have set the New Product to: 
-        </H2>
-    </div>
-    
         <script>
             var ws = new WebSocket(`ws://localhost:8000/current_production`);
             ws.onmessage = function(event) {
@@ -49,6 +35,20 @@ def get_production_overview():
             }
         </script>
 
+    </head>
+    <body>
+
+    <div class="container mt-3">
+        <h1>Current Production</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" class="form-control" id="messageText" autocomplete="off"/>
+            <button class="btn btn-outline-primary mt-2">Send</button>
+        </form>
+        <hr>
+        <H2 id='product' class="mt-5">
+        </H2>
+    </div>
+
     </body>
 </html>
 """
@@ -57,41 +57,52 @@ def get_production_overview():
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-def update_production_overview():
+def get_production_overview():
     try:
         html = """
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>Update Production Overview</title>
-                    </head>
-                    <body>
-                        <h1>Update Current Production</h1>
-                    <form action="" onsubmit="sendMessage(event)">
-                        <input type="text" id="messageText" autocomplete="off"/>
-                        <button>Send</button>
-                    </form>
-                    <ul id='messages'>
-                    </ul>
-                    <script>
-                        var ws = new WebSocket("ws://localhost:8000/production_overview");
-                        ws.onmessage = function(event) {
-                            var messages = document.getElementById('messages')
-                            var message = document.createElement('li')
-                            var content = document.createTextNode(event.data)
-                            message.appendChild(content)
-                            messages.appendChild(message)
-                        };
-                        function sendMessage(event) {
-                            var input = document.getElementById("messageText")
-                            ws.send(input.value)
-                            input.value = ''
-                            event.preventDefault()
-                        }
-                    </script>
-                </body>
-            </html>
-                """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Websocket Demo</title>
+           <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+           <!-- Jquery CDN import  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script>
+            var ws = new WebSocket(`ws://localhost:8000/current_production`);
+            ws.onmessage = function(event) {
+                var new_products = document.getElementById('product')
+                var new_product = document.createElement('H4')
+                var content = document.createTextNode(event.data)
+                new_product.appendChild(content)
+                new_products.appendChild(new_product)
+            };
+
+            $(document).ready(function(){
+              $("button").click(function(){
+                $.get("demo_test.asp", function(data, status){
+                  alert("Data: " + data + "\nStatus: " + status);
+    });
+  });
+});
+        </script>
+
+
+    </head>
+    <body>
+
+    <div class="container mt-3">
+        <h1>Current Production</h1>
+        <hr>
+        <H2 id='product' class="mt-5">
+        You Have set the New Product to: 
+        </H2>
+    </div>
+    
+    </body>
+</html>
+"""
+
         return html
     except Exception as ex:
         print("Data could not be processed: \n", ex)
