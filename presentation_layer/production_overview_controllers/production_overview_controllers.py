8fc019ba2f24911ad389ba0cc0302a312cc76f7f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2023-12-11 14:04:49 +0000

    Added improved HTML for updating the product currently in production

--- presentation_layer/production_overview_controllers/production_overview_controllers.py
+++ presentation_layer/production_overview_controllers/production_overview_controllers.py
@@ -10,34 +10,49 @@ def get_production_overview():
     try:
 
         html = """
-            <!DOCTYPE html>
-            <html>
-                    <head>
-                        <title>Production Overview</title>
-                    </head>
-                    <body>
-                        <h1>Current Production</h1>
-                        <ul id='messages'>
-                        </ul>
-                    <script>
-                        var ws = new WebSocket("ws://localhost:8000/production_overview");
 
-                        ws.onopen = function(event) {
-                        console.log(event)
-                        console.log(event.data)
-                        };
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Websocket Demo</title>
+           <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
 
-                        ws.onmessage = function(event) {
-                            var messages = document.getElementById('messages')
-                            var message = document.createElement('li')
-                            var content = document.createTextNode(event.data)
-                            message.appendChild(content)
-                            messages.appendChild(message)
-                        };
-                    </script>
-                </body>
-            </html>
-            """
+    <div class="container mt-3">
+        <h1>Current Production</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" class="form-control" id="messageText" autocomplete="off"/>
+            <button class="btn btn-outline-primary mt-2">Send</button>
+        </form>
+        <hr>
+        <H2 id='product' class="mt-5">
+        You Have set the New Product to: 
+        </H2>
+    </div>
+    
+        <script>
+            var ws = new WebSocket(`ws://localhost:8000/current_production`);
+            ws.onmessage = function(event) {
+                var new_products = document.getElementById('product')
+                var new_product = document.createElement('H4')
+                var content = document.createTextNode(event.data)
+                new_product.appendChild(content)
+                new_products.appendChild(new_product)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+
+    </body>
+</html>
+"""
+
         return html
     except Exception as ex:
         print("Data could not be processed: \n", ex)
