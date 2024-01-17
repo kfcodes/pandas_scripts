13de8b29eea1b58714f1f51ca3de76ba5427d44a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-01-17 21:16:37 +0000

    Improved the formatting for printing lables API routes

--- routes/label_routes.py
+++ routes/label_routes.py
@@ -8,25 +8,30 @@ label_router = APIRouter();
 async def print_small_product_label_function(id: int):
     await print_small_product_label(id);
     return "PRINTED LABEL"
+
 @label_router.post("/print_large_product_label/{id}")
 async def print_large_product_label_function(id: int, body: Request):
     body =  await body.json();
     quantity = int(body["quantity"])
     await print_large_product_label(id, quantity);
     return "PRINTED LABEL"
+
 @label_router.post("/print_large_combined_label")
 async def print_large_combined_label_function(data: Request):
     json_data =  await data.json();
     response = await print_combined_pallet_label(json_data);
     return response;
-@label_router.post("/print_blank_labels")
+
+@label_router.get("/print_blank_labels")
 async def print_blank_label_function():
     response = await print_blank_pallet_label();
     return response;
+
 @label_router.get("/label/{id}")
 async def print_pallet_label_function_function(id: int):
     response = await print_pallet_label(id);
     return response
+
 @label_router.get("/label_info/{id}")
 async def label_info_function(id:str):
     response = await get_label_info(id);
