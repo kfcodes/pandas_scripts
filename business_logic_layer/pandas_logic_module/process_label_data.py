0d3c3c69ebcdf534e74703f38eaaf959219b3210
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-02-15 15:46:21 +0000

    Added function to process label data from second sheet

--- business_logic_layer/pandas_logic_module/process_label_data.py
+++ business_logic_layer/pandas_logic_module/process_label_data.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 load_dotenv("../../../.env")
 
 def process_label_data(data):
-
     try:
-
         label_data = data.loc[:, [ os.getenv('LABELINPUT1'), os.getenv('LABELINPUT2'), os.getenv('LABELINPUT3'), os.getenv('LABELINPUT4'), os.getenv('LABELINPUT5'), os.getenv('LABELINPUT6'), os.getenv('LABELINPUT7'), os.getenv('LABELINPUT8')]]
 
         label_dataa = label_data[label_data[os.getenv('LABELINPUT8')].notna()]
@@ -14,6 +12,18 @@ def process_label_data(data):
         processed_data = label_dataa.rename(columns={os.getenv('LABELINPUT1'):os.getenv('LABELOUTPUT1'),os.getenv('LABELINPUT2'):os.getenv('LABELOUTPUT2'),os.getenv('LABELINPUT3'):os.getenv('LABELOUTPUT3'),os.getenv('LABELINPUT4'):os.getenv('LABELOUTPUT4'),os.getenv('LABELINPUT5'):os.getenv('LABELOUTPUT5'),os.getenv('LABELINPUT6'):os.getenv('LABELOUTPUT6'),os.getenv('LABELINPUT7'):os.getenv('LABELOUTPUT7'),os.getenv('LABELINPUT8'):os.getenv('LABELOUTPUT8'),})
 
         return(processed_data)
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
 
+def process_label_data_2(data):
+    try:
+        label_data = data.loc[:, [ os.getenv('LABELINPUT1'), os.getenv('LABELINPUT2'), os.getenv('LABELINPUT3'), os.getenv('LABELINPUT4'), os.getenv('LABELINPUT5'), os.getenv('LABELINPUT6'), os.getenv('LABELINPUT7'), os.getenv('LABELINPUT8'), os.getenv('LABELINPUT9')]]
+
+        label_dataa = label_data[label_data[os.getenv('LABELINPUT1')].notna()]
+        label_dataa = label_data[label_data[os.getenv('LABELINPUT8')].notna()]
+
+        processed_data = label_dataa.rename(columns={os.getenv('LABELINPUT1'):os.getenv('LABELOUTPUT1'),os.getenv('LABELINPUT2'):os.getenv('LABELOUTPUT2'),os.getenv('LABELINPUT3'):os.getenv('LABELOUTPUT3'),os.getenv('LABELINPUT4'):os.getenv('LABELOUTPUT4'),os.getenv('LABELINPUT5'):os.getenv('LABELOUTPUT5'),os.getenv('LABELINPUT6'):os.getenv('LABELOUTPUT6'),os.getenv('LABELINPUT7'):os.getenv('LABELOUTPUT7'),os.getenv('LABELINPUT8'):os.getenv('LABELOUTPUT8'),os.getenv('LABELINPUT9'):os.getenv('LABELOUTPUT9'),})
+
+        return(processed_data)
     except Exception as ex:
         print("Data could not be processed: \n", ex)
--- presentation_layer/data_processing_controllers/label_data.py
+++ presentation_layer/data_processing_controllers/label_data.py
@@ -1,5 +1,5 @@
 from business_logic_layer.xlsx_logic_module.read_xlsx_file import read_data_in_sheet
-from business_logic_layer.pandas_logic_module.process_label_data import process_label_data
+from business_logic_layer.pandas_logic_module.process_label_data import process_label_data, process_label_data_2
 from data_access_layer.write_database_functions import update_labels
 
 import os
@@ -10,7 +10,10 @@ def process_label_file():
     try:
         data = read_data_in_sheet('LABELSFILE', os.getenv('SHEETNAME'))
         processed_data = process_label_data(data);
+        data_2 = read_data_in_sheet('LABELSFILE', os.getenv('SHEETNAME_2'))
+        processed_data_2 = process_label_data_2(data_2);
         update_labels(processed_data);
+        update_labels(processed_data_2);
 
     except Exception as ex:
         print("Data could not be processed: \n", ex)
