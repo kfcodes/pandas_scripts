40fb38a254f72efc858d29c9dd8bbb2da8d0e29f
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-13 11:45:37 +0000

    Improved format of label data for large type 4 labels

--- business_logic_layer/zpl_logic_module/box_labels/large_type_4_box_label.py
+++ business_logic_layer/zpl_logic_module/box_labels/large_type_4_box_label.py
@@ -4,12 +4,9 @@ load_dotenv("../../.env")
 
 def create_large_product_label_outline():
     try:
-        zpl_outline = f"""
-^XA^CFD^FWR^LH2,5
-^FO625,20
-^GB5,1160,2^FS
-^FO100,20
-^GB5,1160,2^FS
+        zpl_outline = f"""^XA^CFD^FWR^LH2,5
+^FO625,20^GB5,1160,2^FS
+^FO100,20^GB5,1160,2^FS
 ^FO550,50^A0,40^FDOrdered by:^FS
 ^FO480,50^A0,40^FDProduct Name:^FS
 ^FO410,50^A0,40^FDNet Weight:^FS
@@ -18,7 +15,7 @@ def create_large_product_label_outline():
 ^FO270,50^A0,40^FDLOT:^FS
 ^FO200,50^A0,40^FDMfg. Date:^FS
 ^FO130,50^A0,40^FDMade in:^FS
-^FO30,50^A0,40^FDWeb site:^FS
+^FO35,50^A0,40^FDWeb site:^FS
 """
         data = str(os.getenv("LABELFIELDOUTLINE"))
         zpl = zpl_outline + data
@@ -31,15 +28,13 @@ def create_large_product_label_data(label_info, qty, quantity_in_a_box, exp):
         field_15 = os.getenv("LABELFIELD15")
         field_16 = os.getenv("LABELFIELD16")
         field_11 = os.getenv("LABELFIELD11")
-        zpl = f"""
-^PQ{qty},10,1,Y
-^FO480,400^A0,40^FD{label_info[field_11]}^FS
-^FO410,400^A0,40^FD{quantity_in_a_box}^FS
-^FO340,400^A0,40^FD{label_info[field_16]}^FS
-^FO270,400^A0,40^FD{label_info[field_15]}^FS
-^FO200,400^A0,40^FD{exp}^FS
-^XZ
-"""
+        zpl = f"""^PQ{qty},10,1,Y
+^FO480,300^A0,40^FD{label_info[field_11]}^FS
+^FO410,300^A0,40^FD{quantity_in_a_box}^FS
+^FO340,300^A0,40^FD{label_info[field_16]}^FS
+^FO270,300^A0,40^FD{label_info[field_15]}^FS
+^FO200,300^A0,40^FD{exp}^FS
+^XZ"""
         return(zpl)
     except Exception as ex:
         print("data could not be processed: \n", ex)
