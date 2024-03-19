6e7db09a4c124783515c475da65247821d0b289d
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-19 15:31:51 +0000

    Improved the CRUD API routes for pallet groups

--- routes/pallet_group_crud_routes.py
+++ routes/pallet_group_crud_routes.py
@@ -4,22 +4,22 @@ from presentation_layer.pallet_controllers.pallet_group_crud_controllers import 
 
 pallet_group_crud_router = APIRouter();
 
-@pallet_group_crud_router.post("/pallet_list")
+@pallet_group_crud_router.post("/pallet_group")
 async def create_new_pallet_function():
     pallet_group_id = await create_new_pallet_group()
     return pallet_group_id
 
-@pallet_group_crud_router.get("/pallet_list/{id}")
+@pallet_group_crud_router.get("/pallet_group/{id}")
 async def find_pallet_group_function(id):
     pallet_list = await get_pallet_group_info(id)
     return pallet_list
 
-@pallet_group_crud_router.put("/pallet_list/{id}")
+@pallet_group_crud_router.put("/pallet_group/{id}")
 async def update_pallet_list(id: int, updated_pallet_group_info: Pallet_group):
     pallet_list = await update_pallet_group(id, updated_pallet_group_info)
     return pallet_list
 
-@pallet_group_crud_router.delete("/pallet/{id}")
+@pallet_group_crud_router.delete("/pallet_group/{id}")
 async def delete_pallet_group_function(id):
     pallet_list = await delete_pallet_group(id)
     return pallet_list
