fef303899f894520f3b31e66a21a9a9e9d24ec96
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-19 15:09:04 +0000

    Reflected the new module style in the presentation layer

--- presentation_layer/pallet_controllers/pallet_group_crud_controllers.py
+++ presentation_layer/pallet_controllers/pallet_group_crud_controllers.py
@@ -5,33 +5,33 @@ import os
 from dotenv import load_dotenv
 load_dotenv("../../.env")
 
-async def create_new_pallet_list():
+async def create_new_pallet_group():
     try:
-        db(f"{os.getenv('CREATENEWPALLETLIST')}")
-        pallet_list_info = read_selection_to_list(f"{os.getenv('GETNEWESTPALLETLIST')}")
-        return pallet_list_info
+        db(f"{os.getenv('CREATENEWPALLETGROUP')}")
+        pallet_group_info = read_selection_to_list(f"{os.getenv('GETNEWESTPALLETGROUP')}")
+        return pallet_group_info
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_pallet_list_info(id):
+async def get_pallet_group_info(id):
     try:
-        pallet_list = read_to_list_index(f"{os.getenv('GETPALLET_LIST')}'{int(id)}'")
-        return pallet_list
+        pallet_group = read_to_list_index(f"{os.getenv('GETPALLET_GROUP')}'{int(id)}'")
+        return pallet_group
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def update_pallet_list(id, new_pallet_list_data):
+async def update_pallet_group(id, new_pallet_group_data):
     try:
-        new_pallet_list_data = str(new_pallet_list_data).replace(' ', ' ,').replace('None', 'Null')
-        update_string = str(os.getenv('UPDATEPALLETLIST'))
-        updated_pallet_list_info = db(update_string.format(new_pallet_list_data,id))
-        return updated_pallet_list_info
+        new_pallet_group_data = str(new_pallet_group_data).replace(' ', ' ,').replace('None', 'Null')
+        update_string = str(os.getenv('UPDATEPALLETGROUP'))
+        updated_pallet_group_info = db(update_string.format(new_pallet_group_data,id))
+        return updated_pallet_group_info
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def delete_pallet_list(id):
+async def delete_pallet_group(id):
     try:
-        pallet_list = db(f"{os.getenv('DELETEPALLETLIST')}{id}")
-        return pallet_list
+        pallet_group = db(f"{os.getenv('DELETEPALLETGROUP')}{id}")
+        return pallet_group
     except Exception as ex:
         print("Data could not be processed: \n", ex)
