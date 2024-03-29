8e6f1e54c72f7fe45a8b85945187d71efb373194
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-29 22:16:52 +0000

    Updated the API route to retrieve pallet items on the pallet

--- presentation_layer/pallet_controllers/pallet_item_list_controllers.py
+++ presentation_layer/pallet_controllers/pallet_item_list_controllers.py
@@ -1,6 +1,16 @@
 import os
 from data_access_layer.read_database_functions import read_to_list_index
 
+async def get_all_pallet_items():
+    try:
+        resultlist = [];
+        pallet_items = read_to_list_index(f"{os.getenv('GETALLPALLETITEMS')}")
+        for key, val in pallet_items.items():
+            resultlist.append(val)
+        return resultlist;
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
 async def get_new_pallet_items():
     try:
         resultlist = [];
@@ -11,10 +21,10 @@ async def get_new_pallet_items():
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_all_pallet_items():
+async def find_pallet_items_for_pallet_function(id):
     try:
         resultlist = [];
-        pallet_items = read_to_list_index(f"{os.getenv('GETALLPALLETITEMS')}")
+        pallet_items = read_to_list_index(f"{os.getenv('GETNPALLETITEMSFORPALLET')}{id}")
         for key, val in pallet_items.items():
             resultlist.append(val)
         return resultlist;
--- routes/pallet_item_list_routes.py
+++ routes/pallet_item_list_routes.py
@@ -1,14 +1,19 @@
 from fastapi import APIRouter
-from presentation_layer.pallet_controllers.pallet_item_list_controllers import get_all_pallet_items, get_new_pallet_items
+from presentation_layer.pallet_controllers.pallet_item_list_controllers import get_all_pallet_items, get_new_pallet_items, find_pallet_items_for_pallet_function
 
 pallet_item_list_router = APIRouter();
+
+@pallet_item_list_router.get("/all_pallet_items")
+async def find_all_pallet_items_function():
+    pallet_items = await get_all_pallet_items();
+    return pallet_items
 
 @pallet_item_list_router.get("/new_pallet_items")
 async def find_new_pallet_items_function():
     pallet_items = await get_new_pallet_items();
     return pallet_items
 
-@pallet_item_list_router.get("/all_pallet_items")
-async def find_all_pallet_items_function():
-    pallet_items = await get_all_pallet_items();
+@pallet_item_list_router.get("/pallet_items/{id}")
+async def find_pallet_items_for_function(id):
+    pallet_items = await find_pallet_items_for_pallet_function(id);
     return pallet_items
