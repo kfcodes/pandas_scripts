a59d380e92d5f784ce239a6f416a801e688481be
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-04-03 13:06:46 +0100

    Updated routes for CRUD operations on pallet items

--- routes/pallet_item_crud_routes.py
+++ routes/pallet_item_crud_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from data_models_layer.object_models import Pallet_item
-from presentation_layer.pallet_controllers.pallet_item_crud_controllers import create_pallet_item_with_id, get_items_on_pallet,get_items_on_pallet , update_pallet_item, delete_pallet_item
+from presentation_layer.pallet_controllers.pallet_item_crud_controllers import create_pallet_item_with_id, get_item_on_pallet , update_pallet_item, delete_pallet_item
 
 pallet_item_crud_router = APIRouter();
 
@@ -9,9 +9,9 @@ async def create_pallet_item_function(id):
     item = await create_pallet_item_with_id(id)
     return item
 
-@pallet_item_crud_router.get("/pallet_items/{id}")
-async def find_pallet_items_function(id):
-    items = await get_items_on_pallet(id)
+@pallet_item_crud_router.get("/pallet_item/{id}")
+async def find_pallet_item_function(id):
+    items = await get_item_on_pallet(id)
     return items
 
 @pallet_item_crud_router.put("/pallet_item/{id}")
@@ -21,5 +21,5 @@ async def update_pallet_item_function(id: int, updated_pallet_item_info: Pallet_
 
 @pallet_item_crud_router.delete("/pallet_item/{id}")
 async def delete_pallet_item_function(id):
-    item = await delete_pallet_item(id)
-    return item
+    response = await delete_pallet_item(id)
+    return response
