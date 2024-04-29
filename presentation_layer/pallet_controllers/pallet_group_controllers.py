360e9452a6eedce76d7cf9ba07d8783f9fae1700
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-04-29 13:53:28 +0100

    Added functionality to get pallet summary information from DB

--- presentation_layer/pallet_controllers/pallet_group_controllers.py
+++ presentation_layer/pallet_controllers/pallet_group_controllers.py
@@ -1,8 +1,8 @@
-from data_access_layer.read_database_functions import read_selection_to_list, read_to_list_index
-
 import os
+from data_access_layer.read_database_functions import read_selection_to_list, read_to_list_index
+from data_access_layer.write_database_functions import db, db2
 from dotenv import load_dotenv
-load_dotenv("../../.env")
+load_dotenv(".env")
 
 async def get_recent_pallets():
     try:
@@ -44,6 +44,39 @@ async def get_possible_pallets():
     try:
         pallets = read_selection_to_list(f"{os.getenv('')}")
         return pallets
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+async def get_pallet_summary(like, lot):
+    try:
+        # print(like)
+        like = str(like)
+        # print(lot)
+        lot = str(lot)
+        update_string = str(os.getenv('SUMMARY_1'))
+        # print(update_string)
+        products_string = update_string.format(like,lot)
+        # print(products_string)
+        products = db2(products_string)
+        print(products[0])
+        product_list = [];
+        previous_product = "0";
+        previous_lot = "0";
+        previous_product = "0";
+        for item in products:
+            print(item.product_id)
+            # if (item"product_id"] == previous_product): 
+            #     item["product_description"] = None
+            #     item["product_id"] = None
+            # if (item["lot"] == previous_lot): 
+            #     item["lot"] = None
+            # else:
+            #     previous_product = item["product_id"]
+            #     previous_lot = item["lot"]
+            product_list.append(item)
+        return product_list
+        print(products)
+        # return "product_list"
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
