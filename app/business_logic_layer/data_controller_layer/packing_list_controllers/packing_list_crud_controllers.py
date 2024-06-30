123f9565f5ebd09070d518602189869da1ab1405
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-06-30 16:06:29 +0100

    Updated the data controller layer for CRUD operations on packing lists

---
+++ app/business_logic_layer/data_controller_layer/packing_list_controllers/packing_list_crud_controllers.py
@@ -0,0 +1,50 @@
+from physical_layer.data_access_layer.read_database_functions import read_selection_to_list, read_to_list_index
+from physical_layer.data_access_layer.write_database_functions import db, db2
+
+import os
+from dotenv import load_dotenv
+load_dotenv("../../.env")
+
+# CREATE NEW PACKING LIST
+async def create_new_packing_list(name):
+    try:
+        create_packing_list = str(os.getenv('CREATENEWPACKINGLIST'))
+        result = db(create_packing_list.format(str(name)))
+        print(result)
+        return f"Created new packing list {name}"
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+# GET SUMMARY INFORMATION ABOUT THE PACKING LIST
+async def get_packing_list_info(id):
+    try:
+        packing_list_info = read_to_list_index(f"{os.getenv('GETPACKINGLISTINFO')}'{int(id)}'")
+        return packing_list_info
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+# UPDATE THE NAME OF A PACKING LIST
+async def update_packing_list_name(id, name):
+    try:
+        update_string = str(os.getenv('UPDATEPACKINGLISTNAME'))
+        updated_packing_list_name = db(update_string.format(str(name), int(id)))
+        return updated_packing_list_name;
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+# MARK A PACKING LIST AS DISPATCHED
+async def dispatch_packing_list(id):
+    try:
+        dispatch_string = str(os.getenv('DISPATCHPACKINGLIST'))
+        result = db(dispatch_string.format(int(id)))
+        return result
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
+
+#  DELETE PACKING LIST
+async def delete_packing_list(id):
+    try:
+        result = db(f"{os.getenv('DELETEPACKINGLIST')}{int(id)}")
+        return result
+    except Exception as ex:
+        print("Data could not be processed: \n", ex)
