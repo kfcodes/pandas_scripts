71240045bd233f2bd1ffbd0b6b4d5b15f9a6ff29
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-06-30 16:16:16 +0100

    Updated the data controlllers for retrieving information about packing lists

--- app/business_logic_layer/data_controller_layer/packing_list_controllers/packing_list_controllers.py
+++ app/business_logic_layer/data_controller_layer/packing_list_controllers/packing_list_controllers.py
@@ -5,148 +5,30 @@ import os
 from dotenv import load_dotenv
 load_dotenv(".env")
 
-async def get_recent_pallets():
+async def get_open_packing_lists():
     try:
-        pallets = read_to_list_index(f"{os.getenv('GETRECENTPALLETS')}")
-        resultlist = []
-        for key, val in pallets.items():
-            resultlist.append(val)
-        return resultlist
+        packing_lists = read_selection_to_list(f"{os.getenv('OPENPACKINGLISTS')}")
+        return packing_lists;
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_all_pallets():
+async def get_open_packing_list_names():
     try:
-        pallets = read_to_list_index(f"{os.getenv('GETALLPALLETS')}")
-        resultlist = []
-        for key, val in pallets.items():
-            resultlist.append(val)
-        return resultlist
+        packing_list_names = read_selection_to_list(f"{os.getenv('OPENPACKINGLISTNAMES')}")
+        return packing_list_names;
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_pallet_group(id):
+async def get_pallets_not_on_a_packing_list():
     try:
-        pallets = read_selection_to_list(f"{os.getenv('')}'{id}'")
-        return pallets
+        pallets = read_selection_to_list(f"{os.getenv('PALLETSNOTONPACKINGLIST')}")
+        return pallets;
     except Exception as ex:
         print("Data could not be processed: \n", ex)
 
-async def get_pallet_details(id):
+async def get_packing_list_pallets(id):
     try:
-        pallet_details = read_to_list_index(f"{os.getenv('GETPALLETDETAILS')}'{int(id)}'")
-        pallet_details_array = []
-        pallet_details_array.append(pallet_details[0])
-        return pallet_details_array
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_possible_pallets():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('')}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_pallet_summary(like, lot):
-    try:
-        # print(like)
-        like = str(like)
-        # print(lot)
-        lot = str(lot)
-        pallet_summary = str(os.getenv('SUMMARY_1'))
-        # print(update_string)
-        products_string = pallet_summary.format(like,lot)
-        # print(products_string)
-        products = db2(products_string)
-        # products = products.mappings().all()
-        # print(products)
-        product_list = [];
-        previous_product = "";
-        for item in products:
-            # print(list(item))
-            item = item._asdict()
-            print(item)
-            if (item["product_id"] == previous_product): 
-                item["product_description"] = None
-                item["product_id"] = None
-            else:
-                previous_product = item["product_id"]
-            product_list.append(item)
-        return product_list
-        print(products)
-        # return "product_list"
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_pallet_data():
-    try:
-        pallets = read_to_list_index(f"{os.getenv('GETPALLETDATA')}")
-        print(pallets)
-        pallet_list = [];
-        previousPallet = "0";
-        for key, item in pallets.items():
-            print(item)
-            if (item["PALLET"] == previousPallet): 
-                item["WEIGHT"] = None
-                item["DIMENSIONS"] = None
-                item["PALLET"] = None
-            else:
-                previousPallet = item["PALLET"]
-            # if (item["PALLET"] == previousPallet): 
-            pallet_list.append(item)
-        return pallet_list
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_latest_pallet_data():
-    try:
-        pallets = read_to_list_index(f"{os.getenv('GETLATESTPALLETDATA')}")
-        pallet_list = []
-        for key, val in pallets.items():
-            pallet_list.append(val)
-        return pallet_list
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_picklist():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('')}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_data_for_id(id):
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('GETPALLETDATA')}{id}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_data():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('')}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_open_pallet_groups():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('GETOPENPALLETLISTS')}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_open_pallets():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('GETOPENPALLETS')}")
-        return pallets
-    except Exception as ex:
-        print("Data could not be processed: \n", ex)
-
-async def get_packing_list_pallets():
-    try:
-        pallets = read_selection_to_list(f"{os.getenv('GETPACKINGLISTPALLETS')}")
-        return pallets
+        packing_list_pallets = read_selection_to_list(f"{os.getenv('PACKINGLISTPALLETS')}{int(id)}")
+        return packing_list_pallets;
     except Exception as ex:
         print("Data could not be processed: \n", ex)
