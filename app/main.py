66826133e8282a3172ad5ef143a57bfa3e18517a
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-07-29 16:44:25 +0100

    Added logging configuration files to application

--- app/main.py
+++ app/main.py
@@ -23,13 +23,36 @@ from router_layer.api.v1.alternative_routes.assembly_info_routes import assembly
 # from router_layer.api.v1.production_overview import app as production_overview
 # from router_layer.api.v1.production_schedule_routes import schedule_router
 
+from custom_logging import CustomizeLogger
+from pathlib import Path
+from fastapi import Request
+import uvicorn
+import logging
+
 import os
 from dotenv import load_dotenv
 load_dotenv("../.env")
 
 from business_logic_layer.data_controller_layer.product_controllers.product_controllers import get_product_by_id
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
+
+config_path=Path(__file__).with_name("logging_config.json")
+
+def create_app() -> FastAPI:
+    app = FastAPI(title='CustomLogger', debug=False)
+    logger = CustomizeLogger.make_logger(config_path)
+    app.logger = logger
+    return app
+
+app = create_app()
+
+@app.get('/custom_logger')
+def customize_logger(request: Request):
+    request.app.logger.info("Here Is Your Info Log")
+    a = 1 / 0
+    request.app.logger.error("Here Is Your Error Log")
+    return {'data': "Successfully Implemented Custom Log"}
 
 app.add_middleware(
     CORSMiddleware,
