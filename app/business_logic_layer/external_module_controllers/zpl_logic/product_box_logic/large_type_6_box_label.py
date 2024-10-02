978f77641a54651f42fe94392f757b5ccf63610e
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-10-02 17:11:56 +0100

    Updated the format for the product label information

--- app/business_logic_layer/external_module_controllers/zpl_logic/product_box_logic/large_type_6_box_label.py
+++ app/business_logic_layer/external_module_controllers/zpl_logic/product_box_logic/large_type_6_box_label.py
@@ -9,17 +9,17 @@ def create_large_product_label(data, qty):
 ^XA
 
 ^PQ{qty},10,1,Y
-
-^CF0,50
+ 
+^CF0,45
 ^FO50,30^FD{data["brand"]}^FS
 ^FO220,30^FD{data["product"]}^FS
 ^FO50,85^FD{data["flavour"]}^FS
-
-^CF0,35
+ 
+^CF0,30
 ^FO40,180^FDSKU:        {data["sku"]}^FS
 ^FO40,230^FDBATCH:      {data["eol_lot"]}^FS
 ^FO40,280^FDBB:     {data["eol_bbe"]}^FS
-
+ 
 ^BY2
 ^FO390,180
 ^BCN,120,Y,N,N,N,N
